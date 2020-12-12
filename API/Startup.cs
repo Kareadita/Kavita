a2e6d03d5b1b4059f5b82f5f3089c9ae9453c27f
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2020-12-12 17:03:06 -0600

    Initial commit

---
+++ API/Startup.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+﻿# Kavita
+Kavita, meaning Stories, is a lightweight manga server. The goal is to replace Ubooqity with an 
+open source variant that is flexible and packs more punch, without sacrificing ease to use.
+
+## Goals:
+* Server up Comics (cbr, cbz, zip/rar, pdf, raw images) and Books (epub, mobi, azw, djvu, pdf)
+* Provide Reader for Comics and Books via web app that is responsive
+* Provide customization themes (server installed) for web app
+* Provide hooks into metadata providers to fetch Manga/Comic data
+* Metadata should allow for collections, want to read integration from 3rd party services, genres.
+* Expose an OPDS API/Stream for external readers to use
+* Allow downloading files directly from WebApp
+* WebApp/Server is Translated via Weblate (free for Open Source)
