67b97b3be2e360c7b8d60360bfc4ec17c38ccbd9
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2020-12-15 09:46:45 -0600

    Small cleanup on Account service.

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -60,7 +60,7 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+            var user = await _userRepository.GetUserByUsernameAsync(loginDto.Username);
 
             if (user == null) return Unauthorized("Invalid username");
             
