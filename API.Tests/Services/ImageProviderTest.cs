cd67f1ed58127485a5c52342634a65a6894f8f89
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2021-01-25 15:46:05 -0600

    Comment this code out, this is about to be refactored in next branch.

--- API.Tests/Services/ImageProviderTest.cs
+++ API.Tests/Services/ImageProviderTest.cs
@@ -5,16 +5,16 @@ namespace API.Tests.Services
 {
     public class ImageProviderTest
     {
-        [Theory]
-        [InlineData("v10.cbz", "v10.expected.jpg")]
-        [InlineData("v10 - with folder.cbz", "v10 - with folder.expected.jpg")]
-        [InlineData("v10 - nested folder.cbz", "v10 - nested folder.expected.jpg")]
-        public void GetCoverImageTest(string inputFile, string expectedOutputFile)
-        {
-            var testDirectory = Path.Join(Directory.GetCurrentDirectory(), "../../../Services/Test Data/ImageProvider");
-            var expectedBytes = File.ReadAllBytes(Path.Join(testDirectory, expectedOutputFile));
-            // TODO: Implement this with ScannerService
-            //Assert.Equal(expectedBytes, ImageProvider.GetCoverImage(Path.Join(testDirectory, inputFile)));
-        }
+        // [Theory]
+        // [InlineData("v10.cbz", "v10.expected.jpg")]
+        // [InlineData("v10 - with folder.cbz", "v10 - with folder.expected.jpg")]
+        // [InlineData("v10 - nested folder.cbz", "v10 - nested folder.expected.jpg")]
+        // public void GetCoverImageTest(string inputFile, string expectedOutputFile)
+        // {
+        //     var testDirectory = Path.Join(Directory.GetCurrentDirectory(), "../../../Services/Test Data/ImageProvider");
+        //     var expectedBytes = File.ReadAllBytes(Path.Join(testDirectory, expectedOutputFile));
+        //     // TODO: Implement this with ScannerService
+        //     //Assert.Equal(expectedBytes, ImageProvider.GetCoverImage(Path.Join(testDirectory, inputFile)));
+        // }
     }
 }
