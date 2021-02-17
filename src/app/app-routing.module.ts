cdcfbd4561e458f06c25774de7e578abbaf6a96f
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2021-02-17 11:31:08 -0600

    Restore scroll position to the top on page route change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -40,7 +40,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
