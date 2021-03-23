f85918b5bf9446b5f2b11e72b1bb8fea9daff3cb
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2021-03-23 17:28:48 -0500

    When we scan a library, store the last write time for a file so we can skip page calculation if there were no modifications.

---
+++ API/Data/Migrations/20210323213507_LastModifiedOnMangaFiles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class LastModifiedOnMangaFiles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModified",
+                table: "MangaFile",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastModified",
+                table: "MangaFile");
+        }
+    }
+}
