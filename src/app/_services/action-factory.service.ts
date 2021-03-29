365740fd6d75a4cc4da4e3d8b781c731ef9c0f7d
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2021-03-29 08:30:39 -0500

    Fixed an issue where context menus weren't resetting when an admin logs out and a new non-admin logs in. (#108)

--- src/app/_services/action-factory.service.ts
+++ src/app/_services/action-factory.service.ts
@@ -32,52 +32,23 @@ export class ActionFactoryService {
 
   libraryActions: Array<ActionItem<Library>> = [];
 
-  seriesActions: Array<ActionItem<Series>> = [
-    {
-      action: Action.MarkAsRead,
-      title: 'Mark as Read',
-      callback: this.dummyCallback
-    },
-    {
-      action: Action.MarkAsUnread,
-      title: 'Mark as Unread',
-      callback: this.dummyCallback
-    }
-  ];
+  seriesActions: Array<ActionItem<Series>> = [];
 
-  volumeActions: Array<ActionItem<Volume>> = [
-    {
-      action: Action.MarkAsRead,
-      title: 'Mark as Read',
-      callback: this.dummyCallback
-    },
-    {
-      action: Action.MarkAsUnread,
-      title: 'Mark as Unread',
-      callback: this.dummyCallback
-    }
-  ];
+  volumeActions: Array<ActionItem<Volume>> = [];
 
-  chapterActions: Array<ActionItem<Chapter>> = [
-    {
-      action: Action.MarkAsRead,
-      title: 'Mark as Read',
-      callback: this.dummyCallback
-    },
-    {
-      action: Action.MarkAsUnread,
-      title: 'Mark as Unread',
-      callback: this.dummyCallback
-    }
-  ];
+  chapterActions: Array<ActionItem<Chapter>> = [];
 
   isAdmin = false;
 
   constructor(private accountService: AccountService) {
-    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
+    this.accountService.currentUser$.subscribe(user => {
       if (user) {
         this.isAdmin = this.accountService.hasAdminRole(user);
+      } else {
+        return; // If user is logged out, we don't need to do anything
       }
+
+      this._resetActions();
 
       if (this.isAdmin) {
         this.seriesActions.push({
@@ -95,18 +66,6 @@ export class ActionFactoryService {
         this.seriesActions.push({
           action: Action.Edit,
           title: 'Edit',
-          callback: this.dummyCallback
-        });
-
-        this.volumeActions.push({
-          action: Action.Info,
-          title: 'Info',
-          callback: this.dummyCallback
-        });
-
-        this.chapterActions.push({
-          action: Action.Info,
-          title: 'Info',
           callback: this.dummyCallback
         });
 
@@ -146,4 +105,59 @@ export class ActionFactoryService {
   }
 
   dummyCallback(action: Action, data: any) {}
+
+  _resetActions() {
+    this.seriesActions = [
+      {
+        action: Action.MarkAsRead,
+        title: 'Mark as Read',
+        callback: this.dummyCallback
+      },
+      {
+        action: Action.MarkAsUnread,
+        title: 'Mark as Unread',
+        callback: this.dummyCallback
+      }
+    ];
+
+    this.volumeActions = [
+      {
+        action: Action.MarkAsRead,
+        title: 'Mark as Read',
+        callback: this.dummyCallback
+      },
+      {
+        action: Action.MarkAsUnread,
+        title: 'Mark as Unread',
+        callback: this.dummyCallback
+      }
+    ];
+
+    this.chapterActions = [
+      {
+        action: Action.MarkAsRead,
+        title: 'Mark as Read',
+        callback: this.dummyCallback
+      },
+      {
+        action: Action.MarkAsUnread,
+        title: 'Mark as Unread',
+        callback: this.dummyCallback
+      }
+    ];
+
+    this.volumeActions.push({
+      action: Action.Info,
+      title: 'Info',
+      callback: this.dummyCallback
+    });
+
+    this.chapterActions.push({
+      action: Action.Info,
+      title: 'Info',
+      callback: this.dummyCallback
+    });
+
+
+  }
 }
