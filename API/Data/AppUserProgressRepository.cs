0731732c562cf452f4a15c7a1b6fde36b20deb27
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2021-04-02 08:58:41 -0500

    Added code to remove abandoned progress rows after a chapter gets deleted.

---
+++ API/Data/AppUserProgressRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class AppUserProgressRepository : IAppUserProgressRepository
+    {
+        private readonly DataContext _context;
+
+        public AppUserProgressRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// This will remove any entries that have chapterIds that no longer exists. This will execute the save as well.
+        /// </summary>
+        public async Task<bool> CleanupAbandonedChapters()
+        {
+            //SELECT * from AppUserProgresses as AUP WHERE ChapterId NOT IN (SELECT Id From Chapter);
+            var chapterIds = _context.Chapter.Select(c => c.Id);
+
+            var rowsToRemove = await _context.AppUserProgresses
+                .Where(progress => !chapterIds.Contains(progress.ChapterId))
+                .ToListAsync();
+            
+            _context.RemoveRange(rowsToRemove);
+            return (await _context.SaveChangesAsync()) > 0;
+        }
+    }
+}
