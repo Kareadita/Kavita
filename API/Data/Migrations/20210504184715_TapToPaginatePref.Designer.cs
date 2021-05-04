994d62acae6d5aee1f572f8811b628bd36c10e5a
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2021-05-04 13:50:23 -0500

    Tap to Paginate user setting is implemented. Fixes #193

--- API/Data/Migrations/20210504184715_TapToPaginatePref.Designer.cs
+++ API/Data/Migrations/20210504184715_TapToPaginatePref.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210504184715_TapToPaginatePref")]
+    partial class TapToPaginatePref
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ API/Data/Migrations/20210504184715_TapToPaginatePref.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class TapToPaginatePref : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "BookReaderTapToPaginate",
+                table: "AppUserPreferences",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookReaderTapToPaginate",
+                table: "AppUserPreferences");
+        }
+    }
+}
