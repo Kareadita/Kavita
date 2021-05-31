71556a66628da22f937c70b81b3d6e9c1de1b15d
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2021-05-31 11:27:54 -0500

    Implemented a new Exception type that is for throwing a message to UI without logging in Sentry.

---
+++ Kavita.Common/KavitaException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kavita.Common
+{
+    /// <summary>
+    /// These are used for errors to send to the UI that should not be reported to Sentry
+    /// </summary>
+    [Serializable]
+    public class KavitaException : Exception
+    {
+        public KavitaException()
+        {
+
+        }
+
+        public KavitaException(string message) : base(message)
+        {
+
+        }
+    }
+}
