d00cf7de92cb6f11032a9a9f86f448ac70e33670
Kizaing <kizaing@gmail.com>
2021-06-07 17:04:08 -0400

    Merged Dockerfiles (#282)
    * Dockerfile re-imagined to handle all flavors in one script.

---
+++ copy_runtime.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Copies the correct version of Kavita into the image
+
+set -xv
+
+if [ "$TARGETPLATFORM" == "linux/amd64" ]
+then
+	tar xf /files/kavita-linux-x64.tar.gz -C /
+	mv /Kavita /kavita
+elif [ "$TARGETPLATFORM" == "linux/arm/v7" ]
+then
+	tar xf /files/kavita-linux-arm.tar.gz -C /
+	mv /Kavita /kavita
+elif [ "$TARGETPLATFORM" == "linux/arm64" ]
+then
+	tar xf /files/kavita-linux-arm64.tar.gz -C /
+	mv /Kavita /kavita
+fi
