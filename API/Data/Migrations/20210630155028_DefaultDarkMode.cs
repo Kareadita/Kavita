b6affb7d7ea00086b9d9a173024c434996f20fe2
Robbie Davis <robbie@therobbiedavis.com>
2021-06-30 13:12:26 -0400

    Set dark mode to true and migration
    - Set SiteDarkMode = true in appuserpreference
    - Ran migration for darkmode to be default true in db

---
+++ API/Data/Migrations/20210630155028_DefaultDarkMode.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class DefaultDarkMode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
