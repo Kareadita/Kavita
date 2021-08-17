2b9c27c8c2c44d5a364467efda6789f00eb40f33
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2021-08-17 10:37:10 -0500

    Implemented the code to update RowVersion, which is our concurrency check

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using API.Entities;
 using API.Entities.Interfaces;
 using Microsoft.AspNetCore.Identity;
@@ -67,5 +70,50 @@ namespace API.Data
             if (e.NewState == EntityState.Modified && e.Entry.Entity is IEntityDate entity)
                 entity.LastModified = DateTime.Now;
         }
+
+        private void OnSaveChanges()
+        {
+            var concurrentEntries = base.ChangeTracker.Entries()
+                .Where(x => x.Entity is IHasConcurrencyToken && x.State is EntityState.Added or EntityState.Modified);
+
+            foreach (var entityEntry in concurrentEntries)
+            {
+                var e = (IHasConcurrencyToken)entityEntry.Entity;
+                e.OnSavingChanges();
+            }
+
+        }
+
+        #region SaveChanges overrides
+
+        public override int SaveChanges()
+        {
+            this.OnSaveChanges();
+
+            return base.SaveChanges();
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            this.OnSaveChanges();
+
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            this.OnSaveChanges();
+
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            this.OnSaveChanges();
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        #endregion
     }
 }
