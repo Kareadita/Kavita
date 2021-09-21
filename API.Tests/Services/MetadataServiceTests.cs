8fc83cd271f6f1f346695469fc698f1396141cc9
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2021-09-21 10:31:20 -0500

    Some debugging on GA tests not running

--- API.Tests/Services/MetadataServiceTests.cs
+++ API.Tests/Services/MetadataServiceTests.cs
@@ -15,19 +15,19 @@ namespace API.Tests.Services
     public class MetadataServiceTests
     {
         private readonly string _testDirectory = Path.Join(Directory.GetCurrentDirectory(), "../../../Services/Test Data/ArchiveService/Archives");
-        private readonly string _testCoverImageFile = "thumbnail.jpg";
-        private readonly string _testCoverImageDirectory = Path.Join(Directory.GetCurrentDirectory(), @"..\..\..\Services\Test Data\ArchiveService\CoverImages");
-        private readonly MetadataService _metadataService;
-        private readonly IUnitOfWork _unitOfWork = Substitute.For<IUnitOfWork>();
-        private readonly IImageService _imageService = Substitute.For<IImageService>();
-        private readonly IBookService _bookService = Substitute.For<IBookService>();
-        private readonly IArchiveService _archiveService = Substitute.For<IArchiveService>();
-        private readonly ILogger<MetadataService> _logger = Substitute.For<ILogger<MetadataService>>();
-        private readonly IHubContext<MessageHub> _messageHub = Substitute.For<IHubContext<MessageHub>>();
+        private const string TestCoverImageFile = "thumbnail.jpg";
+        private readonly string _testCoverImageDirectory = Path.Join(Directory.GetCurrentDirectory(), @"../../../Services/Test Data/ArchiveService/CoverImages");
+        //private readonly MetadataService _metadataService;
+        // private readonly IUnitOfWork _unitOfWork = Substitute.For<IUnitOfWork>();
+        // private readonly IImageService _imageService = Substitute.For<IImageService>();
+        // private readonly IBookService _bookService = Substitute.For<IBookService>();
+        // private readonly IArchiveService _archiveService = Substitute.For<IArchiveService>();
+        // private readonly ILogger<MetadataService> _logger = Substitute.For<ILogger<MetadataService>>();
+        // private readonly IHubContext<MessageHub> _messageHub = Substitute.For<IHubContext<MessageHub>>();
 
         public MetadataServiceTests()
         {
-            _metadataService = new MetadataService(_unitOfWork, _logger, _archiveService, _bookService, _imageService, _messageHub);
+            //_metadataService = new MetadataService(_unitOfWork, _logger, _archiveService, _bookService, _imageService, _messageHub);
         }
 
         [Fact]
@@ -107,7 +107,7 @@ namespace API.Tests.Services
         public void ShouldNotUpdateCoverImage_OnSecondRun_CoverImageSet()
         {
             // Represents first run
-            Assert.False(MetadataService.ShouldUpdateCoverImage(_testCoverImageFile, new MangaFile()
+            Assert.False(MetadataService.ShouldUpdateCoverImage(TestCoverImageFile, new MangaFile()
             {
                 FilePath = Path.Join(_testDirectory, "file in folder.zip"),
                 LastModified = new FileInfo(Path.Join(_testDirectory, "file in folder.zip")).LastWriteTime
@@ -118,7 +118,7 @@ namespace API.Tests.Services
         public void ShouldNotUpdateCoverImage_OnSecondRun_HasCoverImage_NoForceUpdate_NoLock()
         {
 
-            Assert.False(MetadataService.ShouldUpdateCoverImage(_testCoverImageFile, new MangaFile()
+            Assert.False(MetadataService.ShouldUpdateCoverImage(TestCoverImageFile, new MangaFile()
             {
                 FilePath = Path.Join(_testDirectory, "file in folder.zip"),
                 LastModified = DateTime.Now
