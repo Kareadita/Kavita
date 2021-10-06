583c961b1c675ce7432e3ed38b32785820bb9582
Robbie Davis <robbie@therobbiedavis.com>
2021-10-06 13:27:56 -0400

    Revert "Potential fix for parsing Cyberpunk 2077"
    This reverts commit a14417e640ddb7ab27f66bcc27ff5ecc41581b25.

--- API/Parser/Parser.cs
+++ API/Parser/Parser.cs
@@ -220,18 +220,6 @@ namespace API.Parser
                 MatchOptions, RegexTimeout),
         };
 
-        private static readonly Regex[] ComicSeriesSpecialCaseRegex = new[]
-{
-            // Cyberpunk 2077 - Your Voice 01
-            new Regex(
-                @"^(?<Series>.+? \d.+) (?<Chapter>(?:[^#])\d+?)",
-                MatchOptions, RegexTimeout),
-            // Cyberpunk 2077 #01
-            new Regex(
-                @"^(?<Series>.+? \d.+) #(?<Chapter>\d.+?)",
-                MatchOptions, RegexTimeout),
-        };
-
         private static readonly Regex[] ComicSeriesRegex = new[]
         {
             // Invincible Vol 01 Family matters (2005) (Digital)
@@ -726,15 +714,7 @@ namespace API.Parser
         }
         public static string ParseComicSeries(string filename)
         {
-            Regex[] ComicCase;
-            if (filename.Contains("Cyberpunk 2077"))
-            {
-                ComicCase = ComicSeriesSpecialCaseRegex;
-            } else
-            {
-                ComicCase = ComicSeriesRegex;
-            }
-            foreach (var regex in ComicCase)
+            foreach (var regex in ComicSeriesRegex)
             {
                 var matches = regex.Matches(filename);
                 foreach (Match match in matches)
