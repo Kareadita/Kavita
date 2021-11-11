2238c1640846e4ecfdcf042a58686668a6e6dfb5
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2021-11-11 17:05:29 -0600

    Change webtoon reader debug mode

--- UI/Web/src/app/manga-reader/infinite-scroller/infinite-scroller.component.ts
+++ UI/Web/src/app/manga-reader/infinite-scroller/infinite-scroller.component.ts
@@ -117,7 +117,7 @@ export class InfiniteScrollerComponent implements OnInit, OnChanges, OnDestroy {
   /**
    * Debug mode. Will show extra information. Use bitwise (|) operators between different modes to enable different output
    */
-  debugMode: DEBUG_MODES = DEBUG_MODES.Outline | DEBUG_MODES.Logs;
+  debugMode: DEBUG_MODES = DEBUG_MODES.None;
 
   get minPageLoaded() {
     return Math.min(...Object.values(this.imagesLoaded));
