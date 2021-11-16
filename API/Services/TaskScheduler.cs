1ada34984fe05bd248d943ee0759022b9c258ee4
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2021-11-16 15:11:17 -0600

    Stats Rework (#765)
    * Fixed a duplicate check for updates. Changed checking from weekly to daily.
    
    * Refactored how dark variables were accessed to reduce size of component css. Refactored Stats code to use lesser information for reporting.
    
    * Use the installId from the database which is most unlikely to change.
    
    * Fixed a missing interface with stat service
    
    * Added DotnetVersion back into collection
    
    * Updated url to new host.

--- API/Services/TaskScheduler.cs
+++ API/Services/TaskScheduler.cs
@@ -74,8 +74,6 @@ namespace API.Services
 
             RecurringJob.AddOrUpdate("cleanup", () => _cleanupService.Cleanup(), Cron.Daily, TimeZoneInfo.Local);
 
-            // Schedule update check between noon and 6pm local time
-            RecurringJob.AddOrUpdate("check-for-updates", () => _scannerService.ScanLibraries(), Cron.Daily(Rnd.Next(12, 18)), TimeZoneInfo.Local);
         }
 
         #region StatsTasks
@@ -91,7 +89,7 @@ namespace API.Services
             }
 
             _logger.LogDebug("Scheduling stat collection daily");
-            RecurringJob.AddOrUpdate(SendDataTask, () => _statsService.Send(), Cron.Daily, TimeZoneInfo.Local);
+            RecurringJob.AddOrUpdate(SendDataTask, () => _statsService.Send(), Cron.Daily(Rnd.Next(0, 22)), TimeZoneInfo.Local);
         }
 
         public void CancelStatsTasks()
@@ -114,8 +112,8 @@ namespace API.Services
         public void ScheduleUpdaterTasks()
         {
             _logger.LogInformation("Scheduling Auto-Update tasks");
-            RecurringJob.AddOrUpdate("check-updates", () => CheckForUpdate(), Cron.Weekly, TimeZoneInfo.Local);
-
+            // Schedule update check between noon and 6pm local time
+            RecurringJob.AddOrUpdate("check-updates", () => _versionUpdaterService.CheckForUpdate(), Cron.Daily(Rnd.Next(12, 18)), TimeZoneInfo.Local);
         }
         #endregion
 
