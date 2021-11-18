ed934cc3817cf543947045e3ef7e3b2627f00ff2
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2021-11-18 08:46:01 -0600

    Removed an empty migration

--- API/Data/Migrations/DataContextModelSnapshot.cs
+++ API/Data/Migrations/DataContextModelSnapshot.cs
@@ -14,7 +14,7 @@ namespace API.Data.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "5.0.10");
+                .HasAnnotation("ProductVersion", "5.0.8");
 
             modelBuilder.Entity("API.Entities.AppRole", b =>
                 {
