fb97b8402bbbe6d0c995b55d87fde6a367b5da60
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2021-11-19 12:25:41 -0600

    Tweaked the scaling for images on horizontal scroll

--- UI/Web/src/app/manga-reader/infinite-scroller/infinite-scroller.component.ts
+++ UI/Web/src/app/manga-reader/infinite-scroller/infinite-scroller.component.ts
@@ -124,7 +124,7 @@ export class InfiniteScrollerComponent implements OnInit, OnChanges, OnDestroy {
   /**
    * Debug mode. Will show extra information. Use bitwise (|) operators between different modes to enable different output
    */
-  debugMode: DEBUG_MODES = DEBUG_MODES.Outline;
+  debugMode: DEBUG_MODES = DEBUG_MODES.Outline | DEBUG_MODES.Logs;
 
   get minPageLoaded() {
     return Math.min(...Object.values(this.imagesLoaded));
@@ -216,7 +216,7 @@ export class InfiniteScrollerComponent implements OnInit, OnChanges, OnDestroy {
     // if (!this.atBottom && this.direction === ScrollDirection.Horizontal) {
     //   event.preventDefault();
     // }
-    //this.handleScrollEvent(event);
+    this.handleScrollEvent(event);
   }
 
   /**
