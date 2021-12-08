1d409d8e9023fb3a79630dab3938411d0c66d472
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2021-12-08 16:52:41 -0600

    Fixed up some potential NPEs during scan

--- API/Data/Metadata/ComicInfo.cs
+++ API/Data/Metadata/ComicInfo.cs
@@ -11,20 +11,20 @@ namespace API.Data.Metadata
     /// <remarks>See reference of the loose spec here: https://github.com/Kussie/ComicInfoStandard/blob/main/ComicInfo.xsd</remarks>
     public class ComicInfo
     {
-        public string Summary { get; set; }
-        public string Title { get; set; }
-        public string Series { get; set; }
-        public string Number { get; set; }
-        public string Volume { get; set; }
-        public string Notes { get; set; }
-        public string Genre { get; set; }
+        public string Summary { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Series { get; set; } = string.Empty;
+        public string Number { get; set; } = string.Empty;
+        public string Volume { get; set; } = string.Empty;
+        public string Notes { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
         public int PageCount { get; set; }
         // ReSharper disable once InconsistentNaming
-        public string LanguageISO { get; set; }
+        public string LanguageISO { get; set; } = string.Empty;
         /// <summary>
         /// This is the link to where the data was scraped from
         /// </summary>
-        public string Web { get; set; }
+        public string Web { get; set; } = string.Empty;
         public int Day { get; set; }
         public int Month { get; set; }
         public int Year { get; set; }
@@ -33,29 +33,23 @@ namespace API.Data.Metadata
         /// <summary>
         /// Rating based on the content. Think PG-13, R for movies. See <see cref="AgeRating"/> for valid types
         /// </summary>
-        public string AgeRating { get; set; }
-
-        // public AgeRating AgeRating
-        // {
-        //     get => ConvertAgeRatingToEnum(_AgeRating);
-        //     set => ConvertAgeRatingToEnum(value);
-        // }
+        public string AgeRating { get; set; } = string.Empty;
         /// <summary>
         /// User's rating of the content
         /// </summary>
         public float UserRating { get; set; }
 
-        public string AlternateSeries { get; set; }
-        public string StoryArc { get; set; }
-        public string SeriesGroup { get; set; }
-        public string AlternativeSeries { get; set; }
-        public string AlternativeNumber { get; set; }
+        public string AlternateSeries { get; set; } = string.Empty;
+        public string StoryArc { get; set; } = string.Empty;
+        public string SeriesGroup { get; set; } = string.Empty;
+        public string AlternativeSeries { get; set; } = string.Empty;
+        public string AlternativeNumber { get; set; } = string.Empty;
 
         /// <summary>
         /// This is Epub only: calibre:title_sort
         /// Represents the sort order for the title
         /// </summary>
-        public string TitleSort { get; set; }
+        public string TitleSort { get; set; } = string.Empty;
 
 
 
@@ -63,14 +57,14 @@ namespace API.Data.Metadata
         /// <summary>
         /// This is the Author. For Books, we map creator tag in OPF to this field. Comma separated if multiple.
         /// </summary>
-        public string Writer { get; set; }
-        public string Penciller { get; set; }
-        public string Inker { get; set; }
-        public string Colorist { get; set; }
-        public string Letterer { get; set; }
-        public string CoverArtist { get; set; }
-        public string Editor { get; set; }
-        public string Publisher { get; set; }
+        public string Writer { get; set; } = string.Empty;
+        public string Penciller { get; set; } = string.Empty;
+        public string Inker { get; set; } = string.Empty;
+        public string Colorist { get; set; } = string.Empty;
+        public string Letterer { get; set; } = string.Empty;
+        public string CoverArtist { get; set; } = string.Empty;
+        public string Editor { get; set; } = string.Empty;
+        public string Publisher { get; set; } = string.Empty;
 
         public static AgeRating ConvertAgeRatingToEnum(string value)
         {
