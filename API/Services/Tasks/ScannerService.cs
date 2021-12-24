4c2c6c32b824f0cffbee3ff0cced5b506b8c351a
Robbie Davis <robbie@therobbiedavis.com>
2021-12-24 10:16:34 -0500

    updating log message

--- API/Services/Tasks/ScannerService.cs
+++ API/Services/Tasks/ScannerService.cs
@@ -224,7 +224,7 @@ public class ScannerService : IScannerService
         // For Docker instances check if any of the folder roots are not available (ie disconnected volumes, etc) and fail if any of them are
         if (library.Folders.Any(f => !_directoryService.IsDirectoryEmpty(f.Path)))
         {
-            _logger.LogError("Some of the root folders for library are empty. Please check that drives are connected and rescan. Scan will be aborted");
+            _logger.LogError("Some of the root folders for the library are empty. Either your mount has been disconnected or you are trying to delete all series in the library. Scan will be aborted. Check that your mount is connected or change the library's root folder and rescan.");
             return;
         }
 
