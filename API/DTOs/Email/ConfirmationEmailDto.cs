98bb6f0b0b7dc2e0d4e542fd20562f90f0fb87ac
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2022-02-05 16:17:51 -0800

    Added in InstallId so that Kavita owned Email service can validate if requests are valid or not. (#1036)

--- API/DTOs/Email/ConfirmationEmailDto.cs
+++ API/DTOs/Email/ConfirmationEmailDto.cs
@@ -5,4 +5,8 @@ public class ConfirmationEmailDto
     public string InvitingUser { get; init; }
     public string EmailAddress { get; init; }
     public string ServerConfirmationLink { get; init; }
+    /// <summary>
+    /// InstallId of this Kavita Instance
+    /// </summary>
+    public string InstallId { get; init; }
 }
--- API/DTOs/Email/PasswordResetEmailDto.cs
+++ API/DTOs/Email/PasswordResetEmailDto.cs
@@ -4,4 +4,8 @@ public class PasswordResetEmailDto
 {
     public string EmailAddress { get; init; }
     public string ServerConfirmationLink { get; init; }
+    /// <summary>
+    /// InstallId of this Kavita Instance
+    /// </summary>
+    public string InstallId { get; init; }
 }
