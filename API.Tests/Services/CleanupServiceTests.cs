8d3973584b10cccd4b58b970edf1d9ec7d8ba0ea
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2022-02-17 12:53:39 -0600

    Fixed unit tests

--- API.Tests/Services/CleanupServiceTests.cs
+++ API.Tests/Services/CleanupServiceTests.cs
@@ -26,7 +26,7 @@ public class CleanupServiceTests
 {
     private readonly ILogger<CleanupService> _logger = Substitute.For<ILogger<CleanupService>>();
     private readonly IUnitOfWork _unitOfWork;
-    private readonly IHubContext<MessageHub> _messageHub = Substitute.For<IHubContext<MessageHub>>();
+    private readonly IEventHub _messageHub = Substitute.For<IEventHub>();
 
     private readonly DbConnection _connection;
     private readonly DataContext _context;
