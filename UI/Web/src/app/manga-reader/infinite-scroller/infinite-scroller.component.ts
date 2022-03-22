a0d7b3caa4c8a4e83cac2b965137e67fc984eec3
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2022-03-22 07:13:58 -0500

    Fixed a bug where continuous reader wasn't being triggered due to moving scrollbar to body and a floating point percision error on scroll top

--- UI/Web/src/app/manga-reader/infinite-scroller/infinite-scroller.component.ts
+++ UI/Web/src/app/manga-reader/infinite-scroller/infinite-scroller.component.ts
@@ -93,7 +93,7 @@ export class InfiniteScrollerComponent implements OnInit, OnChanges, OnDestroy {
   /**
    * The minimum width of images in webtoon. On image loading, this is checked and updated. All images will get this assigned to them for rendering.
    */
-  webtoonImageWidth: number = window.innerWidth || this.document.documentElement.clientWidth || this.document.body.clientWidth;
+  webtoonImageWidth: number = window.innerWidth || this.document.body.clientWidth || this.document.documentElement.clientWidth;
   /**
    * Used to tell if a scrollTo() operation is in progress
    */
@@ -129,7 +129,7 @@ export class InfiniteScrollerComponent implements OnInit, OnChanges, OnDestroy {
   /**
    * Debug mode. Will show extra information. Use bitwise (|) operators between different modes to enable different output
    */
-  debugMode: DEBUG_MODES = DEBUG_MODES.None;
+  debugMode: DEBUG_MODES = DEBUG_MODES.Outline | DEBUG_MODES.Logs;
   /**
    * Debug mode. Will filter out any messages in here so they don't hit the log
    */
@@ -145,7 +145,7 @@ export class InfiniteScrollerComponent implements OnInit, OnChanges, OnDestroy {
 
   get areImagesWiderThanWindow() {
     let [_, innerWidth] = this.getInnerDimensions();
-    return this.webtoonImageWidth > (innerWidth || document.documentElement.clientWidth);
+    return this.webtoonImageWidth > (innerWidth || document.body.clientWidth);
   }
 
 
@@ -230,7 +230,7 @@ export class InfiniteScrollerComponent implements OnInit, OnChanges, OnDestroy {
 
   recalculateImageWidth() {
     const [_, innerWidth] = this.getInnerDimensions();
-    this.webtoonImageWidth = innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    this.webtoonImageWidth = innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
   }
 
   getVerticalOffset() {
@@ -244,8 +244,9 @@ export class InfiniteScrollerComponent implements OnInit, OnChanges, OnDestroy {
     }
 
     return (offset
+      || document.body.scrollTop 
       || document.documentElement.scrollTop 
-      || document.body.scrollTop || 0);
+      || 0);
   }
 
   /**
@@ -290,19 +291,20 @@ export class InfiniteScrollerComponent implements OnInit, OnChanges, OnDestroy {
   getTotalHeight() {
     let totalHeight = 0;
     document.querySelectorAll('img[id^="page-"]').forEach(img => totalHeight += img.getBoundingClientRect().height);
-    return totalHeight;
+    return Math.round(totalHeight);
   }
   getTotalScroll() {
     if (this.isFullscreenMode) {
       return this.readerElemRef.nativeElement.offsetHeight + this.readerElemRef.nativeElement.scrollTop;
     }
-    return document.documentElement.offsetHeight + document.documentElement.scrollTop;
+    return document.body.offsetHeight + document.body.scrollTop;
   }
   getScrollTop() {
     if (this.isFullscreenMode) {
       return this.readerElemRef.nativeElement.scrollTop;
     }
-    return document.documentElement.scrollTop;
+
+    return document.body.scrollTop;
   }
 
   checkIfShouldTriggerContinuousReader() {
@@ -316,14 +318,14 @@ export class InfiniteScrollerComponent implements OnInit, OnChanges, OnDestroy {
       if (this.atTop && this.pageNum > 0) {
         this.atTop = false;
       }
-      
+
       if (totalScroll === totalHeight && !this.atBottom) {
         this.atBottom = true;
         this.setPageNum(this.totalPages);
 
         // Scroll user back to original location
         this.previousScrollHeightMinusTop = this.getScrollTop();
-        requestAnimationFrame(() => document.documentElement.scrollTop = this.previousScrollHeightMinusTop + (SPACER_SCROLL_INTO_PX / 2));
+        requestAnimationFrame(() => document.body.scrollTop = this.previousScrollHeightMinusTop + (SPACER_SCROLL_INTO_PX / 2));
       } else if (totalScroll >= totalHeight + SPACER_SCROLL_INTO_PX && this.atBottom) { 
         // This if statement will fire once we scroll into the spacer at all
         this.loadNextChapter.emit();
@@ -335,7 +337,7 @@ export class InfiniteScrollerComponent implements OnInit, OnChanges, OnDestroy {
 
         this.atTop = true; 
         // Scroll user back to original location
-        this.previousScrollHeightMinusTop = document.documentElement.scrollHeight - document.documentElement.scrollTop;
+        this.previousScrollHeightMinusTop = document.body.scrollHeight - document.body.scrollTop;
         requestAnimationFrame(() => window.scrollTo(0, SPACER_SCROLL_INTO_PX)); // TODO: does this need to be fullscreen protected?
       } else if (this.getScrollTop() < 5 && this.pageNum === 0 && this.atTop) {
         // If already at top, then we moving on
@@ -377,8 +379,8 @@ export class InfiniteScrollerComponent implements OnInit, OnChanges, OnDestroy {
 
     return (rect.bottom >= 0 && 
             rect.right >= 0 && 
-            rect.top <= (innerHeight || document.documentElement.clientHeight) &&
-            rect.left <= (innerWidth || document.documentElement.clientWidth)
+            rect.top <= (innerHeight || document.body.clientHeight) &&
+            rect.left <= (innerWidth || document.body.clientWidth)
           );
   }
 
@@ -398,10 +400,10 @@ export class InfiniteScrollerComponent implements OnInit, OnChanges, OnDestroy {
 
     if (rect.bottom >= 0 && 
             rect.right >= 0 && 
-            rect.top <= (innerHeight || document.documentElement.clientHeight) &&
-            rect.left <= (innerWidth || document.documentElement.clientWidth)
+            rect.top <= (innerHeight || document.body.clientHeight) &&
+            rect.left <= (innerWidth || document.body.clientWidth)
           ) {
-            const topX = (innerHeight || document.documentElement.clientHeight);
+            const topX = (innerHeight || document.body.clientHeight);
             return Math.abs(rect.top / topX) <= 0.25;
           }
     return false;
