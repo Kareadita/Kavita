c54e67ea3aecf24a4e8614130db35c60af82e38f
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2022-04-06 17:10:51 -0500

    Updated dependencies. SharpCompress has been updated to v2.1.0 which should fix pdf rendering on pi/arm64 devices.

--- UI/Web/src/app/dashboard/dashboard.component.ts
+++ UI/Web/src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
-import { ServerService } from '../_services/server.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,23 +9,8 @@ import { ServerService } from '../_services/server.service';
 })
 export class DashboardComponent implements OnInit {
 
-  tabs: Array<{title: string, fragment: string}> = [
-    {title: 'Libraries', fragment: ''},
-    {title: 'Lists', fragment: 'lists'},
-    {title: 'Collections', fragment: 'collections'},
-  ];
-  active = this.tabs[0];
 
-  constructor(public route: ActivatedRoute, private serverService: ServerService, 
-    private toastr: ToastrService, private titleService: Title) {
-    this.route.fragment.subscribe(frag => {
-      const tab = this.tabs.filter(item => item.fragment === frag);
-      if (tab.length > 0) {
-        this.active = tab[0];
-      } else {
-        this.active = this.tabs[0]; // Default to first tab
-      }
-    });
+  constructor(public route: ActivatedRoute, private titleService: Title) {
     this.titleService.setTitle('Kavita - Dashboard');
   }
 
