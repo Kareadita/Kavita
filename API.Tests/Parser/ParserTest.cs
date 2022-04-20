70196e6e4cc200d4791db08f4ff560ed07ee2535
Robbie Davis <robbie@therobbiedavis.com>
2022-04-20 18:05:42 -0400

    Adding gif support (#1225)
    * Adding gif to accepted image extension and unit test
    
    * Revert "Adding gif to accepted image extension and unit test"
    
    This reverts commit d0df8239068ddc12f44aed752804b5db60243e44.
    
    * Adding gif support and unit test
    
    * unit test and event widget
    
    - updating unit test archives to temive unneeded gifs, causing failures
    - adding overflow to event widget

--- API.Tests/Parser/ParserTest.cs
+++ API.Tests/Parser/ParserTest.cs
@@ -179,6 +179,7 @@ namespace API.Tests.Parser
         [InlineData(".test.jpg", false)]
         [InlineData("!test.jpg", true)]
         [InlineData("test.webp", true)]
+        [InlineData("test.gif", true)]
         public void IsImageTest(string filename, bool expected)
         {
             Assert.Equal(expected, IsImage(filename));
