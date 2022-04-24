460704fe8c6afccdb000643d0d45c40d0d8c03a5
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2022-04-24 07:57:27 -0500

    Fixed a bug where we had flipped state on sideNavCollapsed$

--- UI/Web/src/app/sidenav/side-nav/side-nav.component.ts
+++ UI/Web/src/app/sidenav/side-nav/side-nav.component.ts
@@ -62,7 +62,12 @@ export class SideNavComponent implements OnInit, OnDestroy {
       .subscribe((evt: NavigationEnd) => {
         if (this.utilityService.getActiveBreakpoint() < Breakpoint.Tablet) {
           // collapse side nav
-          this.navService.toggleSideNav();
+          this.navService.sideNavCollapsed$.pipe(take(1)).subscribe(collapsed => {
+            console.log('Side nav collapsed: ', collapsed);
+            if (!collapsed) {
+              this.navService.toggleSideNav();
+            }
+          });
         }
       });
   }
