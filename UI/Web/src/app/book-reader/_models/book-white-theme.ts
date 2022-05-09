91f9e3f70501d5e4a0c534fe57e5dd8112ffc24e
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2022-05-09 10:49:28 -0500

    Fixed transparent background for action bar on white theme

--- UI/Web/src/app/book-reader/_models/book-white-theme.ts
+++ UI/Web/src/app/book-reader/_models/book-white-theme.ts
@@ -1,7 +1,8 @@
 // Important note about themes. Must have one section with .reader-container that contains color, background-color and rest of the styles must be scoped to .book-content
 export const BookWhiteTheme = `
-  :root() .brtheme-white {
-    --brtheme-link-text-color: green;
-    --brtheme-bg-color: lightgrey;
+  .reader-container {
+    color: black !important;
+    background-image: none !important;
+    background-color: white !important;
   }
 `;
