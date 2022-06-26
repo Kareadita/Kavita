39f81c230ff2ed300f781ac390e6977a3af77849
tjarls <tjarls@gmail.com>
2022-06-26 23:49:49 +0100

    Add KnownProxies configuration (#1332)

--- API/Startup.cs
+++ API/Startup.cs
@@ -53,8 +53,10 @@ namespace API
             services.AddControllers();
             services.Configure<ForwardedHeadersOptions>(options =>
             {
-                options.ForwardedHeaders =
-                    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+                options.ForwardedHeaders = ForwardedHeaders.All;
+                foreach(var proxy in _config.GetSection("KnownProxies").AsEnumerable().Where(c => c.Value != null)) {
+                    options.KnownProxies.Add(IPAddress.Parse(proxy.Value));
+                }
             });
             services.AddCors();
             services.AddIdentityServices(_config);
@@ -124,13 +126,6 @@ namespace API
             });
 
             services.AddResponseCaching();
-
-            services.Configure<ForwardedHeadersOptions>(options =>
-            {
-                options.ForwardedHeaders =
-                    ForwardedHeaders.All;
-            });
-
 
             services.AddHangfire(configuration => configuration
                 .UseSimpleAssemblyNameTypeSerializer()
@@ -210,10 +205,7 @@ namespace API
 
             app.UseResponseCompression();
 
-            app.UseForwardedHeaders(new ForwardedHeadersOptions
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedHost
-            });
+            app.UseForwardedHeaders();
 
             app.UseRouting();
 
@@ -294,7 +286,6 @@ namespace API
             if (socket.LocalEndPoint is IPEndPoint endPoint) return endPoint.Address.ToString();
             throw new KavitaException("No network adapters with an IPv4 address in the system!");
         }
-
 
     }
 }
