08e23205b1cf33c624d293a856861f13de3da177
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2022-07-02 16:11:10 -0500

    person badge on push

--- UI/Web/src/app/shared/person-badge/person-badge.component.ts
+++ UI/Web/src/app/shared/person-badge/person-badge.component.ts
@@ -1,15 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { Person } from '../../_models/person';
 
 @Component({
   selector: 'app-person-badge',
   templateUrl: './person-badge.component.html',
-  styleUrls: ['./person-badge.component.scss']
+  styleUrls: ['./person-badge.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PersonBadgeComponent implements OnInit {
 
   @Input() person!: Person;
-  
 
   constructor() { }
 
