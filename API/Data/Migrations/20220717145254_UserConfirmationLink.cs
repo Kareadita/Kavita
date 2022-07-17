2bd44b7a117f22e41ebd192f7c06d3798ca4df67
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2022-07-17 10:14:53 -0500

    Implemented a manual button to allow users to setup an account, even after they invited.
    Updated error toast to put "Error" in the title of the toast.

---
+++ API/Data/Migrations/20220717145254_UserConfirmationLink.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class UserConfirmationLink : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConfirmationToken",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfirmationToken",
+                table: "AspNetUsers");
+        }
+    }
+}
