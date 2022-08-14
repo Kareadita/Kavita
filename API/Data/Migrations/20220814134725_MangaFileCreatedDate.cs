01d0ebd00618ca9b79164186f91b6a92c1cd99c6
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2022-08-14 08:57:47 -0500

    Capture date when Kavita creates a MangaFile so we can show the date on the UI.

---
+++ API/Data/Migrations/20220814134725_MangaFileCreatedDate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class MangaFileCreatedDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "MangaFile",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "MangaFile");
+        }
+    }
+}
