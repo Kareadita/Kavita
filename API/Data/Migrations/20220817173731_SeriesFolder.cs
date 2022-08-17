aec3c449d580f9673b4a7f6e825365d689fabfdb
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2022-08-17 12:43:29 -0500

    Added the SeriesFolder migration

---
+++ API/Data/Migrations/20220817173731_SeriesFolder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class SeriesFolder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FolderPath",
+                table: "Series",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastFolderScanned",
+                table: "Series",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FolderPath",
+                table: "Series");
+
+            migrationBuilder.DropColumn(
+                name: "LastFolderScanned",
+                table: "Series");
+        }
+    }
+}
