f0c516ab6203318970aef5729ec3fc7b4ff88c59
tjarls <tjarls@gmail.com>
2022-08-28 22:39:35 +0100

    Sort series by chapter number only when some chapters have no volume (#1487)
    * Sort series by chapter number only when some chapters have no volume information
    
    * Implement a Default static instance of ChapterSortComparer
    
    * Further use Default static Comparers
    
    * Add missing ToLit() as per comments

--- API/Comparators/ChapterSortComparer.cs
+++ API/Comparators/ChapterSortComparer.cs
@@ -23,6 +23,8 @@ namespace API.Comparators
 
             return x.CompareTo(y);
         }
+
+        public static readonly ChapterSortComparer Default = new ChapterSortComparer();
     }
 
     /// <summary>
@@ -44,6 +46,8 @@ namespace API.Comparators
 
             return x.CompareTo(y);
         }
+
+        public static readonly ChapterSortComparerZeroFirst Default = new ChapterSortComparerZeroFirst();
     }
 
     public class SortComparerZeroLast : IComparer<double>
