efdc49d0d45452e7f2956aefab1a3cc50873db20
Ocgineer <rvanbeek1@gmail.com>
2022-09-15 00:45:45 +0200

    added console output on disabled
    Added console.error if Web Animations have been disabled due to the browser not supporting this.

--- UI/Web/src/app/app.module.ts
+++ UI/Web/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { NavModule } from './nav/nav.module';
 
 // Disable Web Animations if the user's browser (such as iOS 12.5.5) does not support this.
 const disableAnimations = !('animate' in document.documentElement);
-
+if (disableAnimations) console.error("Web Animations have been disabled as your current browser does not support this.");
 
 @NgModule({
     declarations: [
