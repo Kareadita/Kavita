b5ac8733493865b7854a2672edfa6c792cc7cc68
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2022-10-31 08:35:33 -0500

    Fixed reading lists promotion not working

--- API/DTOs/ReadingLists/UpdateReadingListDto.cs
+++ API/DTOs/ReadingLists/UpdateReadingListDto.cs
@@ -1,10 +1,14 @@
-﻿namespace API.DTOs.ReadingLists;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs.ReadingLists;
 
 public class UpdateReadingListDto
 {
+    [Required]
     public int ReadingListId { get; set; }
-    public string Title { get; set; }
-    public string Summary { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Summary { get; set; } = string.Empty;
     public bool Promoted { get; set; }
     public bool CoverImageLocked { get; set; }
 }
