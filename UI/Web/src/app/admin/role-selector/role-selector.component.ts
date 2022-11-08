c211f2d115b108ea1bf88bcb747eb8a01a3e270f
Joe Milazzo <josephmajora@gmail.com>
2022-11-08 16:44:30 -0500

    Bugfix/UI fixes (#1645)
    * Moved role selector to OnPush and made it so we disable all other roles when admin is selected.
    
    * When there are no collections, don't point the user to the wiki unless they are an admin
    
    * Fixed a bad piece of code for getPage in reader

--- UI/Web/src/app/admin/role-selector/role-selector.component.ts
+++ UI/Web/src/app/admin/role-selector/role-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Member } from 'src/app/_models/member';
 import { User } from 'src/app/_models/user';
@@ -8,7 +8,8 @@ import { MemberService } from 'src/app/_services/member.service';
 @Component({
   selector: 'app-role-selector',
   templateUrl: './role-selector.component.html',
-  styleUrls: ['./role-selector.component.scss']
+  styleUrls: ['./role-selector.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RoleSelectorComponent implements OnInit {
 
@@ -23,9 +24,9 @@ export class RoleSelectorComponent implements OnInit {
   @Output() selected: EventEmitter<string[]> = new EventEmitter<string[]>();
 
   allRoles: string[] = [];
-  selectedRoles: Array<{selected: boolean, data: string}> = [];
+  selectedRoles: Array<{selected: boolean, disabled: boolean, data: string}> = [];
 
-  constructor(public modal: NgbActiveModal, private accountService: AccountService, private memberService: MemberService) { }
+  constructor(public modal: NgbActiveModal, private accountService: AccountService, private readonly cdRef: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.accountService.getRoles().subscribe(roles => {
@@ -36,8 +37,9 @@ export class RoleSelectorComponent implements OnInit {
       roles = roles.filter(item => !bannedRoles.includes(item));
       this.allRoles = roles;
       this.selectedRoles = roles.map(item => {
-        return {selected: false, data: item};
+        return {selected: false, disabled: false, data: item};
       });
+      this.cdRef.markForCheck();
       this.preselect();
       this.selected.emit(this.selectedRoles.filter(item => item.selected).map(item => item.data));
     });
@@ -51,11 +53,25 @@ export class RoleSelectorComponent implements OnInit {
           foundRole[0].selected = true;
         }
       });
+      this.cdRef.markForCheck();
     }
   }
 
   handleModelUpdate() {
-    this.selected.emit(this.selectedRoles.filter(item => item.selected).map(item => item.data));
+    const roles = this.selectedRoles.filter(item => item.selected).map(item => item.data);
+    if (roles.filter(r => r === 'Admin').length > 0) {
+      // Disable all other items as Admin is selected
+      this.selectedRoles.filter(item => item.data !== 'Admin').forEach(e => {
+        e.disabled = true;
+      });
+    } else {
+      // Re-enable everything
+      this.selectedRoles.forEach(e => {
+        e.disabled = false;
+      });
+    }
+    this.cdRef.markForCheck();
+    this.selected.emit(roles);
   }
 
 }
