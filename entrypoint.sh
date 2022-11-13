c47cf5f31eb9b8b346d5db6f3c82520b881b69ac
Muggz <mug@passw0rd.org>
2022-11-12 17:23:39 -0800

    Added PUID, PGID and KAVITAUSER variable support in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,21 @@
 #! /bin/bash
 
-if [ ! -f "/kavita/config/appsettings.json" ]; then
+# Set default username, UID and GID for Kavita but allow overrides
+PUID=${PUID:-1000}
+PGID=${PGID:-1000}
+KAVITAUSER=${KAVITAUSER:-kavita}
+
+# Add Kavita group if it doesn't already exist
+if [[ $(getent group "$PGID" | cut -d':' -f1) != "$KAVITAUSER" ]]; then
+    groupadd -o -g "$PGID" "$KAVITAUSER"
+fi
+
+# Add Kavita user if it doesn't already exist
+if [[ $(getent passwd "$PUID" | cut -d':' -f1) != "$KAVITAUSER" ]]; then
+    useradd -o -u "$PUID" -g "$PGID" -d /kavita "$KAVITAUSER"
+fi
+
+if [ ! -f /kavita/config/appsettings.json" ]; then
     echo "Kavita configuration file does not exist, creating..."
     echo '{
   "TokenKey": "super secret unguessable key",
@@ -8,6 +23,7 @@ if [ ! -f "/kavita/config/appsettings.json" ]; then
 }' >> /kavita/config/appsettings.json
 fi
 
-chmod +x Kavita
+chown -R "$PUID":"$PGID" /kavita
+chmod 0500 /kavita/Kavita
 
-./Kavita
+su -l "$KAVITAUSER" -c ./Kavita
