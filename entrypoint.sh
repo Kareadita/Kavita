ee14080673f4e2a44d0f086e267dd6f0639dd4c9
Muggz <mug@passw0rd.org>
2022-11-13 15:54:22 -0800

    Only chown config folder and only if needed

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,8 +28,19 @@ if [[ "$PUID" -eq 0 ]]; then
     # Run as root
     ./Kavita
 else
-    # Set ownership on all files except the library if running non-root
-    find /kavita -path /kavita/library -prune -o -exec chown "$PUID":"$PGID" {} \;
+    # Set ownership on config dir if running non-root and current ownership is different
+    if [[ ! "$(stat -c %u /kavita/config)" = "$PUID" ]]; then
+        echo "Specified PUID differs from Kavita config dir ownership, updating permissions now..."
+        if [[ ! "$(stat -c %g /kavita/config)" = "$PGID" ]]; then
+            chown -R "$PUID":"$PGID" /kavita/config
+        else
+            chown -R "$PUID" /kavita/config
+        fi
+
+    elif [[ ! "$(stat -c %g /kavita/config)" = "$PGID" ]]; then
+        echo "Specified PGID differs from Kavita config dir ownership, updating permissions now..."
+        chgrp -R "$PGID" /kavita/config
+    fi
 
     # Run as non-root user
     su -l kavita -c ./Kavita
