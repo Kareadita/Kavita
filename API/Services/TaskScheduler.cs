7e711f37e356ff12f3df268c0d924931d73d9aa0
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2022-12-15 09:45:41 -0600

    Changed the initial stat report to be in 1 day, to avoid people trying and ditching the software from muddying up the stats.

--- API/Services/TaskScheduler.cs
+++ API/Services/TaskScheduler.cs
@@ -147,6 +147,7 @@ public class TaskScheduler : ITaskScheduler
     /// <summary>
     /// First time run stat collection. Executes immediately on a background thread. Does not block.
     /// </summary>
+    /// <remarks>Schedules it for 1 day in the future to ensure we don't have users that try the software out</remarks>
     public async Task RunStatCollection()
     {
         var allowStatCollection  = (await _unitOfWork.SettingsRepository.GetSettingsDtoAsync()).AllowStatCollection;
@@ -155,7 +156,7 @@ public class TaskScheduler : ITaskScheduler
             _logger.LogDebug("User has opted out of stat collection, not sending stats");
             return;
         }
-        BackgroundJob.Enqueue(() => _statsService.Send());
+        BackgroundJob.Schedule(() => _statsService.Send(), DateTimeOffset.Now.AddDays(1));
     }
 
     public void ScanSiteThemes()
