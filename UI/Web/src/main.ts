ef55029a13ddf4aaf053aca6c37ab4b28e7a1e18
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2023-01-30 12:15:10 -0600

    Fixed build system again by deleting nodejs. New build system uses package-lcok going forward.

--- UI/Web/src/main.ts
+++ UI/Web/src/main.ts
@@ -1,22 +1,7 @@
 /// <reference types="@angular/localize" />
 
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-// function fetchConfig(): Promise<ConfigData> {
-//   return fetch(environment.apiUrl + 'settings/base-url')
-//     .then(response => response.text())
-//     .then(response => new ConfigData(response));
-// }
-
-// fetchConfig().then(config => {
-  
-// });
 platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.error(err));
