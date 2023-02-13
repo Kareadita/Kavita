ef1422cf42e632db6757cdff4e9f36cde5b32eb5
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2023-02-13 12:54:25 -0600

    Fixed a bug where manage collections wasn't named correctly in UI.

--- UI/Web/src/app/_models/library.ts
+++ UI/Web/src/app/_models/library.ts
@@ -15,5 +15,5 @@ export interface Library {
     includeInDashboard: boolean;
     includeInRecommended: boolean;
     includeInSearch: boolean;
-    manageCollections: boolean;
+    createCollections: boolean;
 }
