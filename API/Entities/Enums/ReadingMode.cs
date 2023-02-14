1cd5bbe99ebc1c5d85277b43f3c1fd065355424d
CKolle <cl.kolle@stud.uis.no>
2023-02-14 19:30:02 +0100

    Added Description()] annotation for the ReadingMode enum, like other enums, and added summary documentation

--- API/Entities/Enums/ReadingMode.cs
+++ API/Entities/Enums/ReadingMode.cs
@@ -1,7 +1,20 @@
+using System.ComponentModel;
+
 namespace API.Entities.Enums;
 
+/// <summary>
+/// Represents the reading modes for the book-reader
+/// </summary>
 public enum ReadingMode
 {
+    /// <summary>
+    /// Vertical reading mode for the book-reader
+    /// </summary>
+    [Description ("Vertically")]
     Vertically = 0,
+    /// <summary>
+    /// Horizontal reading mode for the book-reader
+    /// </summary>
+    [Description ("Horizontally")]
     Horizontally = 1
 }
