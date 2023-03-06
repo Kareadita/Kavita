56243758d4bec16e5d0c39d8134beddf0b1bc494
Robbie Davis <robbie@therobbiedavis.com>
2023-03-06 14:36:17 -0500

    fix (#1855)

--- API/Program.cs
+++ API/Program.cs
@@ -192,6 +192,7 @@ public class Program
                             }
                         }
                     }
+                });
 
                     webBuilder.UseStartup<Startup>();
             });
