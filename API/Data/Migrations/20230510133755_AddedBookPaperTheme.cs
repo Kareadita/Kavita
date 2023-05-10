04c01483dd99a1f515635448a74e11da36ea680d
Robbie Davis <robbie@therobbiedavis.com>
2023-05-10 09:39:31 -0400

    Adding migrations

---
+++ API/Data/Migrations/20230510133755_AddedBookPaperTheme.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedBookPaperTheme : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
