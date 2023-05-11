c9b16a18def16cf2939c498fe33e1bbff4265191
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2023-05-11 14:21:41 -0500

    Render out the web link

--- UI/Web/src/app/series-detail/_components/series-metadata-detail/series-metadata-detail.component.ts
+++ UI/Web/src/app/series-detail/_components/series-metadata-detail/series-metadata-detail.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { ReaderService } from 'src/app/_services/reader.service';
 import { TagBadgeCursor } from '../../../shared/tag-badge/tag-badge.component';
@@ -9,6 +9,7 @@ import { ReadingList } from '../../../_models/reading-list';
 import { Series } from '../../../_models/series';
 import { SeriesMetadata } from '../../../_models/metadata/series-metadata';
 import { MetadataService } from '../../../_services/metadata.service';
+import { ImageService } from 'src/app/_services/image.service';
 
 
 @Component({
@@ -30,6 +31,8 @@ export class SeriesMetadataDetailComponent implements OnChanges {
   isCollapsed: boolean = true;
   hasExtendedProperites: boolean = false;
 
+  imageService = inject(ImageService);
+
   /**
    * Html representation of Series Summary
    */
@@ -45,6 +48,10 @@ export class SeriesMetadataDetailComponent implements OnChanges {
 
   get FilterQueryParam() {
     return FilterQueryParam;
+  }
+
+  get WebLinks() {
+    return this.seriesMetadata?.webLinks.split(',') || [];
   }
 
   constructor(public utilityService: UtilityService, public metadataService: MetadataService, 
