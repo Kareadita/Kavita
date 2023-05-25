2377460db657a90a4f3b97e192c7b05585fb2df6
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2023-05-25 06:53:42 -0500

    Started an idea for Scrobble History, might rework into generic TaskHistory.

---
+++ API/Entities/ScrobbleHistory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using API.Services.Plus;
+
+namespace API.Entities;
+
+/// <summary>
+/// Represents when Scrobble processing took place
+/// </summary>
+public class ScrobbleHistory
+{
+    public int Id { get; set; }
+    public ScrobbleProvider Provider { get; set; }
+    public DateTime LastRanUtc { get; set; }
+
+}
