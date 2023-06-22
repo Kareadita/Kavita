2b32f73f39e3addf0e2a764319c504bbbcb6de50
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2023-06-22 12:13:50 -0500

    Started building out external reviews.

---
+++ API/Controllers/RatingController.cs
@@ -0,0 +1,23 @@
+﻿using API.Services.Plus;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+/// <summary>
+/// Responsible for providing external ratings for Series
+/// </summary>
+public class RatingController : BaseApiController
+{
+    private readonly ILicenseService _licenseService;
+
+    public RatingController(ILicenseService licenseService)
+    {
+        _licenseService = licenseService;
+    }
+
+    [HttpGet]
+    public ActionResult<int> GetRating(int seriesId)
+    {
+        return Ok(10);
+    }
+}
