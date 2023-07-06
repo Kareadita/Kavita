70e27bb5f391795a096fe2c09aa4321c381826d9
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2023-07-06 14:08:58 -0500

    Fixed an issue where tooltips weren't styled correctly after updating a library. Migrated Library access modal to OnPush.

--- UI/Web/src/app/admin/_modals/library-access-modal/library-access-modal.component.ts
+++ UI/Web/src/app/admin/_modals/library-access-modal/library-access-modal.component.ts
@@ -1,19 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { Library } from 'src/app/_models/library';
-import { Member } from 'src/app/_models/auth/member';
-import { LibraryService } from 'src/app/_services/library.service';
-import { SelectionModel } from 'src/app/typeahead/_components/typeahead.component';
-import { NgFor, NgIf } from '@angular/common';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, Input, OnInit} from '@angular/core';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {Library} from 'src/app/_models/library';
+import {Member} from 'src/app/_models/auth/member';
+import {LibraryService} from 'src/app/_services/library.service';
+import {SelectionModel} from 'src/app/typeahead/_components/typeahead.component';
+import {NgFor, NgIf} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-// TODO: Change to OnPush
 @Component({
-    selector: 'app-library-access-modal',
-    templateUrl: './library-access-modal.component.html',
-    styleUrls: ['./library-access-modal.component.scss'],
-    standalone: true,
-    imports: [ReactiveFormsModule, FormsModule, NgFor, NgIf]
+  selector: 'app-library-access-modal',
+  templateUrl: './library-access-modal.component.html',
+  styleUrls: ['./library-access-modal.component.scss'],
+  standalone: true,
+  imports: [ReactiveFormsModule, FormsModule, NgFor, NgIf],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LibraryAccessModalComponent implements OnInit {
 
@@ -22,6 +22,8 @@ export class LibraryAccessModalComponent implements OnInit {
   selectedLibraries: Array<{selected: boolean, data: Library}> = [];
   selections!: SelectionModel<Library>;
   selectAll: boolean = false;
+
+  cdRef = inject(ChangeDetectorRef);
 
   get hasSomeSelected() {
     return this.selections != null && this.selections.hasSomeSelected();
@@ -53,7 +55,7 @@ export class LibraryAccessModalComponent implements OnInit {
 
   setupSelections() {
     this.selections = new SelectionModel<Library>(false, this.allLibraries);
-      
+
     // If a member is passed in, then auto-select their libraries
     if (this.member !== undefined) {
       this.member.libraries.forEach(lib => {
@@ -61,6 +63,7 @@ export class LibraryAccessModalComponent implements OnInit {
       });
       this.selectAll = this.selections.selected().length === this.allLibraries.length;
     }
+    this.cdRef.markForCheck();
   }
 
   reset() {
@@ -70,6 +73,7 @@ export class LibraryAccessModalComponent implements OnInit {
   toggleAll() {
     this.selectAll = !this.selectAll;
     this.allLibraries.forEach(s => this.selections.toggle(s, this.selectAll));
+    this.cdRef.markForCheck();
   }
 
   handleSelection(item: Library) {
@@ -80,6 +84,7 @@ export class LibraryAccessModalComponent implements OnInit {
     } else if (numberOfSelected == this.selectedLibraries.length) {
       this.selectAll = true;
     }
+    this.cdRef.markForCheck();
   }
 
 }
