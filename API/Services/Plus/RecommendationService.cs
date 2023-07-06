fdf8523a3d234c1cbaea3b6fe7c855e198024797
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2023-07-06 17:20:35 -0500

    When combining external recs, normalize names to filter out differences, like capitalization.

--- API/Services/Plus/RecommendationService.cs
+++ API/Services/Plus/RecommendationService.cs
@@ -9,7 +9,9 @@ using API.DTOs.Recommendation;
 using API.DTOs.Scrobbling;
 using API.Entities;
 using API.Entities.Enums;
+using API.Extensions;
 using API.Helpers;
+using API.Services.Tasks.Scanner.Parser;
 using Flurl.Http;
 using Kavita.Common;
 using Kavita.Common.EnvironmentInfo;
@@ -114,7 +116,7 @@ public class RecommendationService : IRecommendationService
         await _unitOfWork.SeriesRepository.AddSeriesModifiers(userId, recDto.OwnedSeries);
 
         recDto.OwnedSeries = recDto.OwnedSeries.DistinctBy(s => s.Id).OrderBy(r => r.Name).ToList();
-        recDto.ExternalSeries = recDto.ExternalSeries.DistinctBy(s => s.Name).OrderBy(r => r.Name).ToList();
+        recDto.ExternalSeries = recDto.ExternalSeries.DistinctBy(s => s.Name.ToNormalized()).OrderBy(r => r.Name).ToList();
 
         return recDto;
     }
