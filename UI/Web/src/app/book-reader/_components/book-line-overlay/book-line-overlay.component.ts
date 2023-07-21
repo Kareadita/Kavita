af96a9b7ac0d31dc2e82512e2bd87381b668308a
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2023-07-21 15:31:53 -0500

    Fixed up closing the overlay when bookmark mode is active

--- UI/Web/src/app/book-reader/_components/book-line-overlay/book-line-overlay.component.ts
+++ UI/Web/src/app/book-reader/_components/book-line-overlay/book-line-overlay.component.ts
@@ -14,7 +14,6 @@ import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 import getBoundingClientRect from "@popperjs/core/lib/dom-utils/getBoundingClientRect";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {ReaderService} from "../../../_services/reader.service";
-import {ScrollService} from "../../../_services/scroll.service";
 
 enum BookLineOverlayMode {
   None = 0,
@@ -49,7 +48,6 @@ export class BookLineOverlayComponent implements OnInit {
   private readonly destroyRef = inject(DestroyRef);
   private readonly cdRef = inject(ChangeDetectorRef);
   private readonly readerService = inject(ReaderService);
-  private readonly scrollService = inject(ScrollService);
 
   get BookLineOverlayMode() { return BookLineOverlayMode; }
   constructor(private elementRef: ElementRef) {}
@@ -59,22 +57,14 @@ export class BookLineOverlayComponent implements OnInit {
     if (this.parent) {
       fromEvent<MouseEvent>(this.parent.nativeElement, 'mouseup')
         .pipe(takeUntilDestroyed(this.destroyRef),
-          filter((evt: MouseEvent) => {
-            // Shouldn't be within the component
-            const xpath = this.readerService.getXPathTo(evt.target, true);
-            return !xpath.includes('APP-BOOK-LINE-OVERLAY');
-          }),
           tap((event: MouseEvent) => {
             const selection = window.getSelection();
             if (!event.target) return;
 
-            console.log('mouseup: ', event.target, selection);
-
-            if (this.mode !== BookLineOverlayMode.None && !selection) {
+            if (this.mode !== BookLineOverlayMode.None && (!selection || selection.toString().trim() === '')) {
               this.reset();
               return;
             }
-
 
             this.selectedText = selection ? selection.toString().trim() : '';
 
@@ -84,25 +74,15 @@ export class BookLineOverlayComponent implements OnInit {
                 const range = selection!.getRangeAt(0)
                 const rect = range.getBoundingClientRect();
                 const box = getBoundingClientRect(event.target as Element);
-
-                console.log('box: ', box);
-                console.log('rect: ', rect);
-                console.log('range: ', range);
-                console.log('event:', event)
-
                 this.xPath = this.readerService.getXPathTo(event.target);
                 if (this.xPath !== '') {
                   this.xPath = '//' + this.xPath;
                 }
-                console.log('xPath: ', this.xPath)
 
-                this.scrollService.scrollPosition
                 this.overlayPosition = {
                   top: rect.top + window.scrollY - 64 - rect.height, // 64px is the top menu area
-                  //top: box.top + this.scrollService.scrollPosition, // 64px is the top menu area
                   left: rect.left + window.scrollX + 30 // Adjust 10 to center the overlay box horizontally
                 };
-                console.log('positioning at: ', this.overlayPosition);
               }
             }
             this.cdRef.markForCheck();
@@ -121,7 +101,6 @@ export class BookLineOverlayComponent implements OnInit {
   }
 
   createPTOC() {
-    console.log('creating bookmark');
     this.readerService.createPersonalToC(this.libraryId, this.seriesId, this.volumeId, this.chapterId, this.pageNumber,
       this.bookmarkForm.get('name')?.value, this.xPath).pipe(catchError(err => {
         this.focusOnBookmarkInput();
@@ -139,7 +118,6 @@ export class BookLineOverlayComponent implements OnInit {
   }
 
   reset() {
-    console.log('Resetting overlay');
     this.bookmarkForm.reset();
     this.mode = BookLineOverlayMode.None;
     this.xPath = '';
