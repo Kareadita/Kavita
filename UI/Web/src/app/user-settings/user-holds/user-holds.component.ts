a37a56b7edff275467cac7f44c15a37b2930463c
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2023-07-28 11:05:10 -0500

    Converted user-holds.component.html

--- UI/Web/src/app/user-settings/user-holds/user-holds.component.ts
+++ UI/Web/src/app/user-settings/user-holds/user-holds.component.ts
@@ -11,11 +11,12 @@ import {
   NgbAccordionDirective, NgbAccordionHeader,
   NgbAccordionItem
 } from "@ng-bootstrap/ng-bootstrap";
+import {TranslocoModule} from "@ngneat/transloco";
 
 @Component({
   selector: 'app-user-holds',
   standalone: true,
-  imports: [CommonModule, ScrobbleEventTypePipe, NgbAccordionDirective, NgbAccordionCollapse, NgbAccordionBody, NgbAccordionItem, NgbAccordionHeader],
+    imports: [CommonModule, ScrobbleEventTypePipe, NgbAccordionDirective, NgbAccordionCollapse, NgbAccordionBody, NgbAccordionItem, NgbAccordionHeader, TranslocoModule],
   templateUrl: './user-holds.component.html',
   styleUrls: ['./user-holds.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
