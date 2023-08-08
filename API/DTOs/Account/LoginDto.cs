7adc01ea0bbe2206a15bc59d85104fa9f8a90fc2
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2023-08-08 10:52:39 -0500

    Added a comment

--- API/DTOs/Account/LoginDto.cs
+++ API/DTOs/Account/LoginDto.cs
@@ -4,5 +4,8 @@ public class LoginDto
 {
     public string Username { get; init; } = default!;
     public string Password { get; set; } = default!;
+    /// <summary>
+    /// If ApiKey is passed, will ignore username/password for validation
+    /// </summary>
     public string? ApiKey { get; set; } = default!;
 }
