17409dd2a70797414d5197f299a8359759986780
Joe Milazzo <josephmajora@gmail.com>
2023-08-10 08:43:56 -0500

    Shakeout (#2195)
    * Fixed some localization issues. Fixed double slash on base url.
    
    * Weblate Changes (#2194)
    
    * Translated using Weblate (Spanish)
    
    Currently translated at 40.9% (585 of 1427 strings)
    
    Translation: Kavita/ui
    Translate-URL: https://hosted.weblate.org/projects/kavita/ui/es/
    
    * Translated using Weblate (Spanish)
    
    Currently translated at 40.9% (585 of 1427 strings)
    
    Translation: Kavita/ui
    Translate-URL: https://hosted.weblate.org/projects/kavita/ui/es/
    
    * Translated using Weblate (Dutch)
    
    Currently translated at 48.5% (693 of 1427 strings)
    
    Translation: Kavita/ui
    Translate-URL: https://hosted.weblate.org/projects/kavita/ui/nl/
    
    * Translated using Weblate (Portuguese)
    
    Currently translated at 96.8% (153 of 158 strings)
    
    Translation: Kavita/backend
    Translate-URL: https://hosted.weblate.org/projects/kavita/backend/pt/
    
    * Translated using Weblate (Portuguese)
    
    Currently translated at 24.3% (348 of 1427 strings)
    
    Translation: Kavita/ui
    Translate-URL: https://hosted.weblate.org/projects/kavita/ui/pt/
    
    * Translated using Weblate (Spanish)
    
    Currently translated at 3.7% (6 of 158 strings)
    
    Translation: Kavita/backend
    Translate-URL: https://hosted.weblate.org/projects/kavita/backend/es/
    
    * Translated using Weblate (Italian)
    
    Currently translated at 11.2% (161 of 1427 strings)
    
    Translation: Kavita/ui
    Translate-URL: https://hosted.weblate.org/projects/kavita/ui/it/
    
    ---------
    
    Co-authored-by: ThePromidius <thepromidiusyt@gmail.com>
    Co-authored-by: gallegonovato <fran-carro@hotmail.es>
    Co-authored-by: Hans Kalisvaart <hans.kalisvaart@gmail.com>
    Co-authored-by: Duarte Silva <smallflake@protonmail.com>
    Co-authored-by: Tomas Battistini <tomas.battistini@gmail.com>
    
    ---------
    
    Co-authored-by: Weblate (bot) <hosted@weblate.org>
    Co-authored-by: ThePromidius <thepromidiusyt@gmail.com>
    Co-authored-by: gallegonovato <fran-carro@hotmail.es>
    Co-authored-by: Hans Kalisvaart <hans.kalisvaart@gmail.com>
    Co-authored-by: Duarte Silva <smallflake@protonmail.com>
    Co-authored-by: Tomas Battistini <tomas.battistini@gmail.com>

--- UI/Web/src/app/admin/manage-library/manage-library.component.ts
+++ UI/Web/src/app/admin/manage-library/manage-library.component.ts
@@ -118,7 +118,7 @@ export class ManageLibraryComponent implements OnInit {
   }
 
   async deleteLibrary(library: Library) {
-    if (await this.confirmService.confirm(translate('toast.confirm-library-delete', {name: library.name}))) {
+    if (await this.confirmService.confirm(translate('toasts.confirm-library-delete', {name: library.name}))) {
       this.deletionInProgress = true;
       this.libraryService.delete(library.id).pipe(take(1)).subscribe(() => {
         this.deletionInProgress = false;
