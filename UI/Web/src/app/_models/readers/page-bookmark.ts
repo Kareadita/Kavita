ea4571b3d6f4eabe7db1f2125d42ee777bf289eb
Joe Milazzo <josephmajora@gmail.com>
2023-09-03 06:42:55 -0700

    Last Shakeout before Release (#2253)
    * [skip ci] Weblate Changes (#2250)
    
    * [skip ci] Translated using Weblate (Korean)
    
    Currently translated at 100.0% (1486 of 1486 strings)
    
    Translation: Kavita/ui
    Translate-URL: https://hosted.weblate.org/projects/kavita/ui/ko/
    
    * [skip ci] Translated using Weblate (Chinese (Simplified))
    
    Currently translated at 100.0% (1486 of 1486 strings)
    
    Translation: Kavita/ui
    Translate-URL: https://hosted.weblate.org/projects/kavita/ui/zh_Hans/
    
    * Translated using Weblate (Chinese (Simplified))
    
    Currently translated at 100.0% (161 of 161 strings)
    
    Translation: Kavita/backend
    Translate-URL: https://hosted.weblate.org/projects/kavita/backend/zh_Hans/
    
    * Translated using Weblate (Korean)
    
    Currently translated at 100.0% (161 of 161 strings)
    
    Translation: Kavita/backend
    Translate-URL: https://hosted.weblate.org/projects/kavita/backend/ko/
    
    * Translated using Weblate (Czech)
    
    Currently translated at 100.0% (161 of 161 strings)
    
    Translation: Kavita/backend
    Translate-URL: https://hosted.weblate.org/projects/kavita/backend/cs/
    
    * [skip ci] Translated using Weblate (Czech)
    
    Currently translated at 15.7% (234 of 1486 strings)
    
    Translation: Kavita/ui
    Translate-URL: https://hosted.weblate.org/projects/kavita/ui/cs/
    
    * [skip ci] Translated using Weblate (Spanish)
    
    Currently translated at 88.5% (1316 of 1486 strings)
    
    Translation: Kavita/ui
    Translate-URL: https://hosted.weblate.org/projects/kavita/ui/es/
    
    * Translated using Weblate (Spanish)
    
    Currently translated at 100.0% (161 of 161 strings)
    
    Translation: Kavita/backend
    Translate-URL: https://hosted.weblate.org/projects/kavita/backend/es/
    
    * [skip ci] Translated using Weblate (Dutch)
    
    Currently translated at 72.2% (1073 of 1486 strings)
    
    Translation: Kavita/ui
    Translate-URL: https://hosted.weblate.org/projects/kavita/ui/nl/
    
    * Translated using Weblate (Italian)
    
    Currently translated at 100.0% (161 of 161 strings)
    
    Translation: Kavita/backend
    Translate-URL: https://hosted.weblate.org/projects/kavita/backend/it/
    
    * [skip ci] Translated using Weblate (Italian)
    
    Currently translated at 100.0% (1486 of 1486 strings)
    
    Translation: Kavita/ui
    Translate-URL: https://hosted.weblate.org/projects/kavita/ui/it/
    
    ---------
    
    Co-authored-by: LeeWan1210 <dldhks456@live.com>
    Co-authored-by: aleixcox <18121624@qq.com>
    Co-authored-by: Jiří Heger <jiri.heger@gmail.com>
    Co-authored-by: gallegonovato <fran-carro@hotmail.es>
    Co-authored-by: Hans Kalisvaart <hans.kalisvaart@gmail.com>
    Co-authored-by: Florestano Pepe <florestano.pepe@gmail.com>
    
    * Fixed bookmark page not sorting and ordering correctly.
    
    ---------
    
    Co-authored-by: Weblate (bot) <hosted@weblate.org>
    Co-authored-by: LeeWan1210 <dldhks456@live.com>
    Co-authored-by: aleixcox <18121624@qq.com>
    Co-authored-by: Jiří Heger <jiri.heger@gmail.com>
    Co-authored-by: gallegonovato <fran-carro@hotmail.es>
    Co-authored-by: Hans Kalisvaart <hans.kalisvaart@gmail.com>
    Co-authored-by: Florestano Pepe <florestano.pepe@gmail.com>

--- UI/Web/src/app/_models/readers/page-bookmark.ts
+++ UI/Web/src/app/_models/readers/page-bookmark.ts
@@ -1,8 +1,10 @@
+import {Series} from "../series";
+
 export interface PageBookmark {
     id: number;
     page: number;
     seriesId: number;
     volumeId: number;
     chapterId: number;
-    fileName: string;
+    series: Series;
 }
