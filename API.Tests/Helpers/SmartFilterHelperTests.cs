bc0cb7c80790148f5de0f2ee339153fe2d9c2e44
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2023-09-03 09:25:23 -0500

    Started working on the decode code. I added encode, but I don't think it's needed since the UI can do it.

---
+++ API.Tests/Helpers/SmartFilterHelperTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using API.DTOs.Filtering;
+using API.DTOs.Filtering.v2;
+using API.Entities.Enums;
+using API.Helpers;
+using Xunit;
+
+namespace API.Tests.Helpers;
+
+public class SmartFilterHelperTests
+{
+    [Fact]
+    public void Test_Decode()
+    {
+        var encoded = """
+                      stmts=comparison%3D5%26field%3D18%26value%3D6%2Ccomparison%3D0%26field%3D4%26value%3D0%2Ccomparison%3D7%26field%3D1%26value%3Da&sortOptions=sortField=1&isAscending=true&limitTo=0&combination=1
+                      """;
+
+        var filter = SmartFilterHelper.Decode(encoded);
+
+        Assert.Equal(0, filter.LimitTo);
+        Assert.Equal(SortField.SortName, filter.SortOptions.SortField);
+        Assert.True(filter.SortOptions.IsAscending);
+        Assert.Null(filter.Name);
+
+        var list = filter.Statements.ToList();
+        AssertStatementSame(list[2], FilterField.SeriesName, FilterComparison.Matches, "a");
+        AssertStatementSame(list[1], FilterField.AgeRating, FilterComparison.Equal, (int) AgeRating.Unknown + "");
+        AssertStatementSame(list[0], FilterField.Genres, FilterComparison.Contains, "6");
+
+    }
+
+    private void AssertStatementSame(FilterStatementDto statement, FilterField field, FilterComparison combination, string value)
+    {
+        Assert.Equal(statement.Field, field);
+        Assert.Equal(statement.Comparison, combination);
+        Assert.Equal(statement.Value, value);
+    }
+
+}
---
+++ API/Helpers/SmartFilterHelper.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using API.DTOs.Filtering;
+using API.DTOs.Filtering.v2;
+
+namespace API.Helpers;
+
+public static class SmartFilterHelper
+{
+    private const string SortOptionsKey = "sortOptions=";
+    private const string StatementsKey = "stmts=";
+    private const string LimitToKey = "limitTo=";
+    private const string CombinationKey = "combination=";
+
+    public static FilterV2Dto Decode(string encodedFilter)
+    {
+        if (string.IsNullOrWhiteSpace(encodedFilter))
+        {
+            return new FilterV2Dto(); // Create a default filter if the input is empty
+        }
+
+        string[] parts = encodedFilter.Split('&');
+        var filter = new FilterV2Dto();
+
+        foreach (var part in parts)
+        {
+            if (part.StartsWith(SortOptionsKey))
+            {
+                filter.SortOptions = DecodeSortOptions(part.Substring(SortOptionsKey.Length));
+            }
+            else if (part.StartsWith(LimitToKey))
+            {
+                filter.LimitTo = int.Parse(part.Substring(LimitToKey.Length));
+            }
+            else if (part.StartsWith(CombinationKey))
+            {
+                filter.Combination = Enum.Parse<FilterCombination>(part.Split("=")[1]);
+            }
+            else if (part.StartsWith(StatementsKey))
+            {
+                filter.Statements = DecodeFilterStatementDtos(part.Substring(StatementsKey.Length));
+            }
+            else if (part.StartsWith("name="))
+            {
+                filter.Name = HttpUtility.UrlDecode(part.Substring(5));
+            }
+        }
+
+        return filter;
+    }
+
+    public static string Encode(FilterV2Dto filter)
+    {
+        if (filter == null)
+            return "";
+
+        var encodedStatements = EncodeFilterStatementDtos(filter.Statements);
+        var encodedSortOptions = filter.SortOptions != null
+            ? $"{SortOptionsKey}{EncodeSortOptions(filter.SortOptions)}"
+            : "";
+        var encodedLimitTo = $"{LimitToKey}{filter.LimitTo}";
+
+        return $"{EncodeName(filter.Name)}{encodedStatements}&{encodedSortOptions}&{encodedLimitTo}&{CombinationKey}{filter.Combination}";
+    }
+
+    private static string EncodeName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            return "";
+
+        return $"name={HttpUtility.UrlEncode(name)}&";
+    }
+
+    private static string EncodeSortOptions(SortOptions sortOptions)
+    {
+        return $"sortField={sortOptions.SortField}&isAscending={sortOptions.IsAscending}";
+    }
+
+    private static string EncodeFilterStatementDtos(ICollection<FilterStatementDto> statements)
+    {
+        if (statements == null || statements.Count == 0)
+            return string.Empty;
+
+        var encodedStatements = StatementsKey + string.Join(",", statements.Select(EncodeFilterStatementDto));
+        return HttpUtility.UrlEncode(encodedStatements);
+    }
+
+    private static string EncodeFilterStatementDto(FilterStatementDto statement)
+    {
+        var encodedComparison = $"comparison={statement.Comparison}";
+        var encodedField = $"field={statement.Field}";
+        var encodedValue = $"value={HttpUtility.UrlEncode(statement.Value)}";
+
+        return $"{encodedComparison}&{encodedField}&{encodedValue}";
+    }
+
+    private static List<FilterStatementDto> DecodeFilterStatementDtos(string encodedStatements)
+    {
+        encodedStatements = HttpUtility.UrlDecode(encodedStatements);
+        string[] statementStrings = encodedStatements.Split(',');
+
+        var statements = new List<FilterStatementDto>();
+
+        foreach (var statementString in statementStrings)
+        {
+            var parts = statementString.Split('&');
+            if (parts.Length < 3)
+                continue;
+
+            statements.Add(new FilterStatementDto
+            {
+                Comparison = Enum.Parse<FilterComparison>(parts[0].Split("=")[1]),
+                Field = Enum.Parse<FilterField>(parts[1].Split("=")[1]),
+                Value = HttpUtility.UrlDecode(parts[2].Split("=")[1])
+            });
+        }
+
+        return statements;
+    }
+
+    private static SortOptions DecodeSortOptions(string encodedSortOptions)
+    {
+        string[] parts = encodedSortOptions.Split('&');
+        var sortFieldPart = parts.FirstOrDefault(part => part.StartsWith("sortField="));
+        var isAscendingPart = parts.FirstOrDefault(part => part.StartsWith("isAscending="));
+
+        var isAscending = isAscendingPart?.Substring(11).Equals("true", StringComparison.OrdinalIgnoreCase) ?? true;
+        if (sortFieldPart != null)
+        {
+            var sortField = Enum.Parse<SortField>(sortFieldPart.Split("=")[1]);
+
+            return new SortOptions
+            {
+                SortField = sortField,
+                IsAscending = isAscending
+            };
+        }
+
+        return null;
+    }
+}
