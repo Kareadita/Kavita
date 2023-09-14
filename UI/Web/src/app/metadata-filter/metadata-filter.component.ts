6c544de8a3380334d4f44e0975c9eb42bd928a2e
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2023-09-14 07:08:39 -0500

    Trying to switch over to a typeahead for the filters, but need library changes.
    Changed so that selected options from the new typeahead wont show as an option anymore.

--- UI/Web/src/app/metadata-filter/metadata-filter.component.ts
+++ UI/Web/src/app/metadata-filter/metadata-filter.component.ts
@@ -30,6 +30,8 @@ import {MetadataService} from "../_services/metadata.service";
 import {FilterUtilitiesService} from "../shared/_services/filter-utilities.service";
 import {FilterService} from "../_services/filter.service";
 import {ToastrService} from "ngx-toastr";
+import {Select2Module, Select2Option, Select2UpdateEvent} from "ng-select2-component";
+import {SmartFilter} from "../_models/metadata/v2/smart-filter";
 
 @Component({
     selector: 'app-metadata-filter',
@@ -38,7 +40,7 @@ import {ToastrService} from "ngx-toastr";
     changeDetection: ChangeDetectionStrategy.OnPush,
     standalone: true,
   imports: [NgIf, NgbCollapse, NgTemplateOutlet, DrawerComponent, NgbTooltip, TypeaheadComponent,
-    ReactiveFormsModule, FormsModule, NgbRating, AsyncPipe, TranslocoModule, SortFieldPipe, MetadataBuilderComponent, NgForOf]
+    ReactiveFormsModule, FormsModule, NgbRating, AsyncPipe, TranslocoModule, SortFieldPipe, MetadataBuilderComponent, NgForOf, Select2Module]
 })
 export class MetadataFilterComponent implements OnInit {
 
@@ -78,16 +80,25 @@ export class MetadataFilterComponent implements OnInit {
   allSortFields = allSortFields;
   allFilterFields = allFields;
 
-  handleFilters(filter: SeriesFilterV2) {
-    this.filterV2 = filter;
-  }
+  smartFilters!: Array<Select2Option>;
+
+
 
 
   private readonly cdRef = inject(ChangeDetectorRef);
   private readonly toastr = inject(ToastrService);
 
 
-  constructor(public toggleService: ToggleService, private filterService: FilterService) {}
+  constructor(public toggleService: ToggleService, private filterService: FilterService) {
+    this.filterService.getAllFilters().subscribe(res => {
+      this.smartFilters = res.map(r => {
+        return {
+          value: r,
+          label: r.name,
+        }
+      });
+    });
+  }
 
   ngOnInit(): void {
     if (this.filterSettings === undefined) {
@@ -105,6 +116,11 @@ export class MetadataFilterComponent implements OnInit {
 
     this.loadFromPresetsAndSetup();
   }
+
+  updateFilterValue(event: Select2UpdateEvent<any>) {
+    console.log('event: ', event);
+  }
+
 
   close() {
     this.filterOpen.emit(false);
@@ -135,6 +151,10 @@ export class MetadataFilterComponent implements OnInit {
     }
 
     return clonedObj;
+  }
+
+  handleFilters(filter: SeriesFilterV2) {
+    this.filterV2 = filter;
   }
 
 
