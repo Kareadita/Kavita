d98bff9b334003a296c69e709eba2a5e41a4f55c
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2023-10-04 08:41:17 -0500

    Started working on the design. Person badge now supports an image instead of an icon.

--- UI/Web/src/app/_single-module/series-preview-drawer/series-preview-drawer.component.ts
+++ UI/Web/src/app/_single-module/series-preview-drawer/series-preview-drawer.component.ts
@@ -4,11 +4,17 @@ import {TranslocoDirective} from "@ngneat/transloco";
 import {NgbActiveOffcanvas} from "@ng-bootstrap/ng-bootstrap";
 import {ExternalSeriesDetail} from "../../_models/series-detail/external-series-detail";
 import {SeriesService} from "../../_services/series.service";
+import {ImageComponent} from "../../shared/image/image.component";
+import {LoadingComponent} from "../../shared/loading/loading.component";
+import {SafeHtmlPipe} from "../../pipe/safe-html.pipe";
+import {A11yClickDirective} from "../../shared/a11y-click.directive";
+import {MetadataDetailComponent} from "../../series-detail/_components/metadata-detail/metadata-detail.component";
+import {PersonBadgeComponent} from "../../shared/person-badge/person-badge.component";
 
 @Component({
   selector: 'app-series-preview-drawer',
   standalone: true,
-  imports: [CommonModule, TranslocoDirective],
+  imports: [CommonModule, TranslocoDirective, ImageComponent, LoadingComponent, SafeHtmlPipe, A11yClickDirective, MetadataDetailComponent, PersonBadgeComponent],
   templateUrl: './series-preview-drawer.component.html',
   styleUrls: ['./series-preview-drawer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -20,6 +26,8 @@ export class SeriesPreviewDrawerComponent implements OnInit {
   @Input() malId?: number;
   @Input({required: true}) isExternalSeries: boolean = true;
 
+  isLoading: boolean = true;
+
   private readonly activeOffcanvas = inject(NgbActiveOffcanvas);
   private readonly seriesService = inject(SeriesService);
   private readonly cdRef = inject(ChangeDetectorRef);
@@ -27,6 +35,7 @@ export class SeriesPreviewDrawerComponent implements OnInit {
   ngOnInit() {
     this.seriesService.getExternalSeriesDetails(this.aniListId, this.malId).subscribe(externalSeries => {
       this.externalSeries = externalSeries;
+      this.isLoading = false;
       console.log('External Series Detail: ', this.externalSeries);
       this.cdRef.markForCheck();
     });
--- UI/Web/src/app/shared/person-badge/person-badge.component.ts
+++ UI/Web/src/app/shared/person-badge/person-badge.component.ts
@@ -1,18 +1,28 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, Input, OnInit} from '@angular/core';
 import { Person } from '../../_models/metadata/person';
 import {CommonModule} from "@angular/common";
+import {SeriesStaff} from "../../_models/series-detail/external-series-detail";
+import {ImageComponent} from "../image/image.component";
 
 @Component({
   selector: 'app-person-badge',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ImageComponent],
   templateUrl: './person-badge.component.html',
   styleUrls: ['./person-badge.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PersonBadgeComponent {
+export class PersonBadgeComponent implements OnInit {
 
-  @Input({required: true}) person!: Person;
+  @Input({required: true}) person!: Person | SeriesStaff;
+  @Input() isStaff = false;
 
-  constructor() { }
+  private readonly cdRef = inject(ChangeDetectorRef);
+
+  staff!: SeriesStaff;
+
+  ngOnInit() {
+    this.staff = this.person as SeriesStaff;
+    this.cdRef.markForCheck();
+  }
 }
