42e5d4608033239db5aaab5ef6c65a9c6fc1e1db
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2023-11-04 07:15:29 -0500

    Tried chartcss with reading activity but it's way too complicated and not as feature rich. Commiting in case I come back, but going to undo this.

--- UI/Web/src/app/statistics/_components/reading-activity/reading-activity.component.ts
+++ UI/Web/src/app/statistics/_components/reading-activity/reading-activity.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, DestroyRef, inject, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, Input, OnInit} from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { filter, map, Observable, of, shareReplay, switchMap } from 'rxjs';
 import { MangaFormatPipe } from 'src/app/pipe/manga-format.pipe';
@@ -11,16 +11,26 @@ import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 import { LineChartModule } from '@swimlane/ngx-charts';
 import { NgIf, NgFor, AsyncPipe } from '@angular/common';
 import {TranslocoDirective, TranslocoService} from "@ngneat/transloco";
+import {UtcToLocalTimePipe} from "../../../pipe/utc-to-local-time.pipe";
+import {tap} from "rxjs/operators";
+import {MangaFormat} from "../../../_models/manga-format";
 
 const options: Intl.DateTimeFormatOptions  = { month: "short", day: "numeric" };
 
+interface DayPoint {
+  count: number, format: MangaFormat
+}
+interface DayGroup {
+  date: string, values: Array<DayPoint>
+}
+
 @Component({
-    selector: 'app-reading-activity',
-    templateUrl: './reading-activity.component.html',
-    styleUrls: ['./reading-activity.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: true,
-  imports: [ReactiveFormsModule, NgIf, NgFor, LineChartModule, AsyncPipe, TranslocoDirective]
+  selector: 'app-reading-activity',
+  templateUrl: './reading-activity.component.html',
+  styleUrls: ['./reading-activity.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [ReactiveFormsModule, NgIf, NgFor, LineChartModule, AsyncPipe, TranslocoDirective, MangaFormatPipe]
 })
 export class ReadingActivityComponent implements OnInit {
   /**
@@ -30,6 +40,11 @@ export class ReadingActivityComponent implements OnInit {
   @Input() isAdmin: boolean = true;
   @Input() individualUserMode: boolean = false;
 
+  private readonly utcDatePipe = new UtcToLocalTimePipe();
+  private readonly destroyRef = inject(DestroyRef);
+  private readonly translocoService = inject(TranslocoService);
+  private readonly cdRef = inject(ChangeDetectorRef);
+
   view: [number, number] = [0, 400];
   formGroup: FormGroup = new FormGroup({
     'users': new FormControl(-1, []),
@@ -37,12 +52,56 @@ export class ReadingActivityComponent implements OnInit {
   });
   users$: Observable<Member[]> | undefined;
   data$: Observable<Array<PieDataItem>>;
+  data2$: Observable<Array<any>>;
   timePeriods = TimePeriods;
-  private readonly destroyRef = inject(DestroyRef);
-  private readonly translocoService = inject(TranslocoService);
+  max: number = 0;
   mangaFormatPipe = new MangaFormatPipe(this.translocoService);
 
+
+
+
   constructor(private statService: StatisticsService, private memberService: MemberService) {
+    this.data2$ = this.formGroup.valueChanges.pipe(
+      switchMap(_ => this.statService.getReadCountByDay(this.formGroup.get('users')!.value, this.formGroup.get('days')!.value)),
+      // tap(data => {
+      //   this.max = data.reduce((acc, day) => Math.max(acc, day.value), 0);
+      //   console.log('max: ', this.max);
+      //   this.cdRef.markForCheck();
+      // }),
+      map(data => {
+
+        // We need the data to be grouped by date
+        const groupedData: any = {};
+
+        data.forEach((item) => {
+          const date = new Date(item.value).toLocaleDateString("en-US", options);
+          if (!groupedData[date]) {
+            groupedData[date] = [];
+          }
+          this.max = Math.max(this.max, item.count);
+          groupedData[date].push({
+            count: item.count,
+            format: item.format,
+          });
+
+        });
+
+        // Convert the grouped data object into an array of objects
+        const result = Object.entries(groupedData)
+        .map(([date, values]) => ({
+          date,
+          values,
+        }));
+
+        console.log(result);
+
+        return result;
+      }),
+      takeUntilDestroyed(this.destroyRef),
+      shareReplay(),
+    );
+
+
     this.data$ = this.formGroup.valueChanges.pipe(
       switchMap(_ => this.statService.getReadCountByDay(this.formGroup.get('users')!.value, this.formGroup.get('days')!.value)),
       map(data => {
