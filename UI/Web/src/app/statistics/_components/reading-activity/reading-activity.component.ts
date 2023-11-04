1ebdd4b2588127a79947d666f6ca615564ae7fd4
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2023-11-04 07:16:23 -0500

    Reverted

--- UI/Web/src/app/statistics/_components/reading-activity/reading-activity.component.ts
+++ UI/Web/src/app/statistics/_components/reading-activity/reading-activity.component.ts
@@ -12,17 +12,9 @@ import { LineChartModule } from '@swimlane/ngx-charts';
 import { NgIf, NgFor, AsyncPipe } from '@angular/common';
 import {TranslocoDirective, TranslocoService} from "@ngneat/transloco";
 import {UtcToLocalTimePipe} from "../../../pipe/utc-to-local-time.pipe";
-import {tap} from "rxjs/operators";
-import {MangaFormat} from "../../../_models/manga-format";
 
 const options: Intl.DateTimeFormatOptions  = { month: "short", day: "numeric" };
 
-interface DayPoint {
-  count: number, format: MangaFormat
-}
-interface DayGroup {
-  date: string, values: Array<DayPoint>
-}
 
 @Component({
   selector: 'app-reading-activity',
@@ -52,56 +44,10 @@ export class ReadingActivityComponent implements OnInit {
   });
   users$: Observable<Member[]> | undefined;
   data$: Observable<Array<PieDataItem>>;
-  data2$: Observable<Array<any>>;
   timePeriods = TimePeriods;
-  max: number = 0;
   mangaFormatPipe = new MangaFormatPipe(this.translocoService);
 
-
-
-
   constructor(private statService: StatisticsService, private memberService: MemberService) {
-    this.data2$ = this.formGroup.valueChanges.pipe(
-      switchMap(_ => this.statService.getReadCountByDay(this.formGroup.get('users')!.value, this.formGroup.get('days')!.value)),
-      // tap(data => {
-      //   this.max = data.reduce((acc, day) => Math.max(acc, day.value), 0);
-      //   console.log('max: ', this.max);
-      //   this.cdRef.markForCheck();
-      // }),
-      map(data => {
-
-        // We need the data to be grouped by date
-        const groupedData: any = {};
-
-        data.forEach((item) => {
-          const date = new Date(item.value).toLocaleDateString("en-US", options);
-          if (!groupedData[date]) {
-            groupedData[date] = [];
-          }
-          this.max = Math.max(this.max, item.count);
-          groupedData[date].push({
-            count: item.count,
-            format: item.format,
-          });
-
-        });
-
-        // Convert the grouped data object into an array of objects
-        const result = Object.entries(groupedData)
-        .map(([date, values]) => ({
-          date,
-          values,
-        }));
-
-        console.log(result);
-
-        return result;
-      }),
-      takeUntilDestroyed(this.destroyRef),
-      shareReplay(),
-    );
-
-
     this.data$ = this.formGroup.valueChanges.pipe(
       switchMap(_ => this.statService.getReadCountByDay(this.formGroup.get('users')!.value, this.formGroup.get('days')!.value)),
       map(data => {
