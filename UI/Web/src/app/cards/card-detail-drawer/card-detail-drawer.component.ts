e2680fc77b8ddc7990158162acba2263bdcba8bb
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2024-01-16 14:32:13 -0600

    Fixed a bad localization string for chapter actions from within the card detail drawer.

--- UI/Web/src/app/cards/card-detail-drawer/card-detail-drawer.component.ts
+++ UI/Web/src/app/cards/card-detail-drawer/card-detail-drawer.component.ts
@@ -160,7 +160,7 @@ export class CardDetailDrawerComponent implements OnInit {
 
     this.chapterActions = this.actionFactoryService.getChapterActions(this.handleChapterActionCallback.bind(this))
                                 .filter(item => item.action !== Action.Edit);
-    this.chapterActions.push({title: 'Read', action: Action.Read, callback: this.handleChapterActionCallback.bind(this), requiresAdmin: false, children: []});
+    this.chapterActions.push({title: 'read', action: Action.Read, callback: this.handleChapterActionCallback.bind(this), requiresAdmin: false, children: []});
     if (this.isChapter) {
       const chapter = this.utilityService.asChapter(this.data);
       this.chapterActions = this.actionFactoryService.filterSendToAction(this.chapterActions, chapter);
