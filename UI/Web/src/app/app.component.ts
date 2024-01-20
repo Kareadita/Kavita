1e73ae546c6be5335cd4759ece1b289150883e46
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2024-01-20 11:20:16 -0600

    Popup to force users to update

--- UI/Web/src/app/app.component.ts
+++ UI/Web/src/app/app.component.ts
@@ -1,18 +1,19 @@
 import {Component, DestroyRef, HostListener, inject, Inject, OnInit} from '@angular/core';
 import { NavigationStart, Router, RouterOutlet } from '@angular/router';
-import {map, pluck, shareReplay, take} from 'rxjs/operators';
+import {map, pluck, shareReplay, take, tap} from 'rxjs/operators';
 import { AccountService } from './_services/account.service';
 import { LibraryService } from './_services/library.service';
 import { NavService } from './_services/nav.service';
 import { filter } from 'rxjs/operators';
 import { NgbModal, NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
 import { DOCUMENT, NgClass, NgIf, AsyncPipe } from '@angular/common';
-import { Observable } from 'rxjs';
+import {interval, Observable} from 'rxjs';
 import {ThemeService} from "./_services/theme.service";
 import { SideNavComponent } from './sidenav/_components/side-nav/side-nav.component';
 import {NavHeaderComponent} from "./nav/_components/nav-header/nav-header.component";
 import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 import {translate, TranslocoService} from "@ngneat/transloco";
+import {UpdateModalComponent} from "./update-modal/update-modal.component";
 
 @Component({
     selector: 'app-root',
@@ -38,13 +39,13 @@ export class AppComponent implements OnInit {
     ratingConfig.resettable = true;
 
     // Close any open modals when a route change occurs
-    router.events
-      .pipe(filter(event => event instanceof NavigationStart), takeUntilDestroyed(this.destroyRef))
-      .subscribe((event) => {
-        if (this.ngbModal.hasOpenModals()) {
-          this.ngbModal.dismissAll();
-        }
-      });
+    // router.events
+    //   .pipe(filter(event => event instanceof NavigationStart), takeUntilDestroyed(this.destroyRef))
+    //   .subscribe((event) => {
+    //     if (this.ngbModal.hasOpenModals()) {
+    //       this.ngbModal.dismissAll();
+    //     }
+    //   });
 
     this.transitionState$ = this.accountService.currentUser$.pipe(map((user) => {
       if (!user) return false;
@@ -62,6 +63,19 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.setDocHeight();
+
+    // Pop an update notice that nags user to update to the new repo
+    this.ngbModal.open(UpdateModalComponent, {size: 'xl'})
+    interval(30 * 60 * 1000) // 30 minutes in milliseconds
+      .pipe(
+        tap(() => {
+          if (!this.ngbModal.hasOpenModals()) {
+            this.ngbModal.open(UpdateModalComponent, {size: 'xl'})
+          }
+        })
+      )
+      .subscribe();
+
     this.setCurrentUser();
   }
 
---
+++ UI/Web/src/app/update-modal/update-modal.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+
+@Component({
+  selector: 'app-update-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './update-modal.component.html',
+  styleUrls: ['./update-modal.component.scss']
+})
+export class UpdateModalComponent {
+
+  constructor(private ngbActiveModal: NgbActiveModal) {}
+
+  close() {
+    this.ngbActiveModal.close();
+  }
+}
