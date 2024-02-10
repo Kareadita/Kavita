7805ae428899ea0a48a55e67303dbb93b2c33665
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2024-02-09 18:32:21 -0600

    Fixed the implementation of the rate limiter and set it up in the ExternalMetadataService.
    Needs to be tested with a new library add.

--- API/Services/Plus/ExternalMetadataService.cs
+++ API/Services/Plus/ExternalMetadataService.cs
@@ -13,6 +13,7 @@ using API.Entities;
 using API.Entities.Enums;
 using API.Entities.Metadata;
 using API.Extensions;
+using API.Helpers;
 using AutoMapper;
 using Flurl.Http;
 using Hangfire;
@@ -76,6 +77,8 @@ public class ExternalMetadataService : IExternalMetadataService
         Ratings = ArraySegment<RatingDto>.Empty,
         Reviews = ArraySegment<UserReviewDto>.Empty
     };
+    // Allow 50 requests per 24 hours
+    private static readonly RateLimiter _rateLimiter = new RateLimiter(10, TimeSpan.FromMinutes(10), false);
 
     public ExternalMetadataService(IUnitOfWork unitOfWork, ILogger<ExternalMetadataService> logger, IMapper mapper, ILicenseService licenseService)
     {
@@ -83,7 +86,6 @@ public class ExternalMetadataService : IExternalMetadataService
         _logger = logger;
         _mapper = mapper;
         _licenseService = licenseService;
-
 
 
         FlurlHttp.ConfigureClient(Configuration.KavitaPlusApiUrl, cli =>
@@ -145,13 +147,31 @@ public class ExternalMetadataService : IExternalMetadataService
         await _unitOfWork.CommitAsync();
     }
 
-    [DisableConcurrentExecution(60 * 60 * 60)]
-    [AutomaticRetry(Attempts = 3, OnAttemptsExceeded = AttemptsExceededAction.Delete)]
-    public Task GetNewSeriesData(int seriesId, LibraryType libraryType)
+    /// <summary>
+    /// Fetches data from Kavita+
+    /// </summary>
+    /// <param name="seriesId"></param>
+    /// <param name="libraryType"></param>
+    public async Task GetNewSeriesData(int seriesId, LibraryType libraryType)
     {
-        // TODO: Implement this task
-        if (!IsPlusEligible(libraryType)) return Task.CompletedTask;
-        return Task.CompletedTask;
+        // TODO: I need this to only allow this to be called 50 times per 24 hours
+        if (!IsPlusEligible(libraryType)) return;
+
+        // Generate key based on seriesId and libraryType or any unique identifier for the request
+        // Check if the request is allowed based on the rate limit
+        if (!_rateLimiter.TryAcquire(string.Empty))
+        {
+            // Request not allowed due to rate limit
+            _logger.LogDebug("Rate Limit hit for Kavita+ prefetch");
+            return;
+        }
+
+        _logger.LogDebug("Prefetching Kavita+ data for Series {SeriesId}", seriesId);
+        // Prefetch SeriesDetail data
+        await GetSeriesDetailPlus(seriesId, libraryType);
+
+        // TODO: Fetch Series Metadata
+
     }
 
     /// <summary>
