18d1f41c71e89b68800704a96047b06afe228a1a
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2024-02-21 09:26:56 -0600

    All migrations except special mixed with loose leaf is done.

---
+++ API/Data/ManualMigrations/ManualMigrateMixedSpecials.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace API.Data.ManualMigrations;
+
+public class UserProgressCsvRecord
+{
+    public bool IsSpecial { get; set; }
+    public int AppUserId { get; set; }
+    public int PagesRead { get; set; }
+    public string Range { get; set; }
+    public string Number { get; set; }
+    public float MinNumber { get; set; }
+    public int SeriesId { get; set; }
+    public int VolumeId { get; set; }
+}
+
+/// <summary>
+/// v0.8.0 migration to move Specials into their own volume and retain user progress.
+/// </summary>
+public class ManualMigrateMixedSpecials
+{
+    public static async Task Migrate(DataContext dataContext, IUnitOfWork unitOfWork, ILogger<Program> logger)
+    {
+        if (await dataContext.ManualMigrationHistory.AnyAsync(m => m.Name == "ManualMigrateMixedSpecials"))
+        {
+            return;
+        }
+
+        logger.LogCritical(
+            "Running ManualMigrateMixedSpecials migration - Please be patient, this may take some time. This is not an error");
+
+        // Get all chapters that are specials with progress
+        // Store them in an csv
+        // Recreate the special volumes
+        var progress = await dataContext.AppUserProgresses
+            .Join(dataContext.Chapter, p => p.ChapterId, c => c.Id, (p, c) => new UserProgressCsvRecord
+            {
+                IsSpecial = c.IsSpecial,
+                AppUserId = p.AppUserId,
+                PagesRead = p.PagesRead,
+                Range = c.Range,
+                Number = c.Number,
+                MinNumber = c.MinNumber,
+                SeriesId = p.SeriesId,
+                VolumeId = p.VolumeId
+            })
+            .Where(d => d.IsSpecial || d.Number == "0")
+            .ToListAsync();
+
+
+        // dataContext.ManualMigrationHistory.Add(new ManualMigrationHistory()
+        // {
+        //     Name = "ManualMigrateMixedSpecials",
+        //     ProductVersion = BuildInfo.Version.ToString(),
+        //     RanAt = DateTime.UtcNow
+        // });
+
+        await dataContext.SaveChangesAsync();
+        logger.LogCritical(
+            "Running ManualMigrateMixedSpecials migration - Completed. This is not an error");
+    }
+}
--- API/Data/ManualMigrations/MigrateChapterFields.cs
+++ API/Data/ManualMigrations/MigrateChapterFields.cs
@@ -1,9 +1,66 @@
-﻿namespace API.Data.ManualMigrations;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using API.Services.Tasks.Scanner.Parser;
+using Kavita.Common.EnvironmentInfo;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
+namespace API.Data.ManualMigrations;
+
+
+
+/// <summary>
+/// Introduced in v0.8.0, this migrates the existing Chapter and Volume 0 -> Parser defined, MangaFile.FileName
+/// </summary>
 public static class MigrateChapterFields
 {
-    // TODO: See if possible to move specials to another volume without loosing progress (potentially exporting to csv then importing by searching on MangaFile path)
-    // Migrate MangaFile to have FileName filled out
+    public static async Task Migrate(DataContext dataContext, IUnitOfWork unitOfWork, ILogger<Program> logger)
+    {
+        if (await dataContext.ManualMigrationHistory.AnyAsync(m => m.Name == "MigrateChapterFields"))
+        {
+            return;
+        }
 
+        logger.LogCritical(
+            "Running MigrateChapterFields migration - Please be patient, this may take some time. This is not an error");
 
+        // Update all volumes only have specials in them (rare)
+        var volumes = dataContext.Volume
+            .Include(v => v.Chapters)
+            .Where(v => v.Name == "0" && v.Chapters.All(c => c.IsSpecial))
+            .ToList();
+        foreach (var volume in volumes)
+        {
+            volume.Number = Parser.SpecialVolumeNumber;
+            volume.MinNumber = Parser.SpecialVolumeNumber;
+            volume.MaxNumber = Parser.SpecialVolumeNumber;
+        }
+
+        // Update all MangaFile
+        foreach (var mangaFile in dataContext.MangaFile)
+        {
+            mangaFile.FileName = Path.GetFileNameWithoutExtension(mangaFile.FilePath);
+        }
+
+        foreach (var chapter in dataContext.Chapter.Where(c => c.Number == "0"))
+        {
+            chapter.Number = Parser.DefaultChapter;
+            chapter.MinNumber = Parser.DefaultChapterNumber;
+            chapter.MaxNumber = Parser.DefaultChapterNumber;
+        }
+
+        dataContext.ManualMigrationHistory.Add(new ManualMigrationHistory()
+        {
+            Name = "MigrateChapterFields",
+            ProductVersion = BuildInfo.Version.ToString(),
+            RanAt = DateTime.UtcNow
+        });
+
+        await dataContext.SaveChangesAsync();
+        logger.LogCritical(
+            "Running MigrateChapterFields migration - Completed. This is not an error");
+    }
 }
