22cd025cee3e447980dd9d914e9ec32906d6c31e
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2024-04-05 10:34:22 -0500

    Changed some of the format icons to some better looking icons.

--- UI/Web/src/app/_pipes/manga-format-icon.pipe.ts
+++ UI/Web/src/app/_pipes/manga-format-icon.pipe.ts
@@ -13,15 +13,15 @@ export class MangaFormatIconPipe implements PipeTransform {
   transform(format: MangaFormat): string {
     switch (format) {
       case MangaFormat.EPUB:
-        return 'fa-book';
+        return 'fa fa-book';
       case MangaFormat.ARCHIVE:
-        return 'fa-file-archive';
+        return 'fa-solid fa-file-zipper';
       case MangaFormat.IMAGE:
-        return 'fa-image';
+        return 'fa-solid fa-file-image';
       case MangaFormat.PDF:
-        return 'fa-file-pdf';
+        return 'fa-solid fa-file-pdf';
       case MangaFormat.UNKNOWN:
-        return 'fa-question';
+        return 'fa-solid fa-file-circle-question';
     }
   }
 
