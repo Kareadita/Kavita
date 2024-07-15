3ee8d8f758544bc1ff32074b0d29446093a7f4ce
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2024-07-15 15:42:10 -0500

    Control flow updates

--- UI/Web/src/app/user-settings/restriction-selector/restriction-selector.component.ts
+++ UI/Web/src/app/user-settings/restriction-selector/restriction-selector.component.ts
@@ -7,7 +7,7 @@ import { AgeRatingDto } from 'src/app/_models/metadata/age-rating-dto';
 import { User } from 'src/app/_models/user';
 import { MetadataService } from 'src/app/_services/metadata.service';
 import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
-import {NgIf, NgFor, TitleCasePipe, NgTemplateOutlet} from '@angular/common';
+import {TitleCasePipe, NgTemplateOutlet} from '@angular/common';
 import {TranslocoModule} from "@ngneat/transloco";
 
 @Component({
@@ -16,7 +16,7 @@ import {TranslocoModule} from "@ngneat/transloco";
     styleUrls: ['./restriction-selector.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
     standalone: true,
-  imports: [NgIf, ReactiveFormsModule, NgFor, NgbTooltip, TitleCasePipe, TranslocoModule, NgTemplateOutlet]
+  imports: [ReactiveFormsModule, NgbTooltip, TitleCasePipe, TranslocoModule, NgTemplateOutlet]
 })
 export class RestrictionSelectorComponent implements OnInit, OnChanges {
 
