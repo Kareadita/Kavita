188599b062193ba4e9ec62d0c5b2231a3300085f
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2024-08-12 12:21:45 -0500

    Changed Cover Artists -> Artists to keep it more generic.
    Updated cast tab so items are clickable and will load a filter on all-series (instead of limited to library)

--- UI/Web/src/app/_single-module/cast-tab/cast-tab.component.ts
+++ UI/Web/src/app/_single-module/cast-tab/cast-tab.component.ts
@@ -1,8 +1,13 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, Input} from '@angular/core';
 import {CarouselReelComponent} from "../../carousel/_components/carousel-reel/carousel-reel.component";
 import {PersonBadgeComponent} from "../../shared/person-badge/person-badge.component";
 import {TranslocoDirective} from "@jsverse/transloco";
 import {IHasCast} from "../../_models/common/i-has-cast";
+import {Person, PersonRole} from "../../_models/metadata/person";
+import {Router} from "@angular/router";
+import {FilterField} from "../../_models/metadata/v2/filter-field";
+import {FilterComparison} from "../../_models/metadata/v2/filter-comparison";
+import {FilterUtilitiesService} from "../../shared/_services/filter-utilities.service";
 
 @Component({
   selector: 'app-cast-tab',
@@ -18,6 +23,18 @@ import {IHasCast} from "../../_models/common/i-has-cast";
 })
 export class CastTabComponent {
 
+  private readonly router = inject(Router);
+  private readonly filterUtilityService = inject(FilterUtilitiesService);
+  protected readonly PersonRole = PersonRole;
+  protected readonly FilterField = FilterField;
+
   @Input({required: true}) metadata!: IHasCast;
+
+
+  openPerson(queryParamName: FilterField, filter: Person) {
+    if (queryParamName === FilterField.None) return;
+    this.filterUtilityService.applyFilter(['all-series'], queryParamName, FilterComparison.Equal, `${filter.id}`).subscribe();
+  }
+
 
 }
