6484d3094d8723212d6005cba4ed5645d69923e1
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2024-12-07 07:52:53 -0600

    Reverted the stats api url

--- API/Services/Tasks/StatsService.cs
+++ API/Services/Tasks/StatsService.cs
@@ -44,7 +44,7 @@ public class StatsService : IStatsService
     private readonly UserManager<AppUser> _userManager;
     private readonly IEmailService _emailService;
     private readonly ICacheService _cacheService;
-    private const string ApiUrl = "http://localhost:5001"; //"https://stats.kavitareader.com"
+    private const string ApiUrl = "https://stats.kavitareader.com";
     private const string ApiKey = "MsnvA2DfQqxSK5jh"; // It's not important this is public, just a way to keep bots from hitting the API willy nilly
 
     public StatsService(ILogger<StatsService> logger, IUnitOfWork unitOfWork, DataContext context,
