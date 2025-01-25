e62eb72f9d6f0ce2e5d88bdf136ea6ee4c1f4c01
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2025-01-25 13:23:59 -0600

    Age Rating mapping is working

--- UI/Web/src/app/admin/manage-metadata-settings/manage-metadata-settings.component.ts
+++ UI/Web/src/app/admin/manage-metadata-settings/manage-metadata-settings.component.ts
@@ -1,6 +1,6 @@
 import {ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit} from '@angular/core';
 import {TranslocoDirective} from "@jsverse/transloco";
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {SettingSwitchComponent} from "../../settings/_components/setting-switch/setting-switch.component";
 import {SettingItemComponent} from "../../settings/_components/setting-item/setting-item.component";
 import {DefaultValuePipe} from "../../_pipes/default-value.pipe";
@@ -9,6 +9,10 @@ import {SettingsService} from "../settings.service";
 import {debounceTime, switchMap, tap} from "rxjs";
 import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 import {map} from "rxjs/operators";
+import {AgeRatingPipe} from "../../_pipes/age-rating.pipe";
+import {AgeRating} from "../../_models/metadata/age-rating";
+import {MetadataService} from "../../_services/metadata.service";
+import {AgeRatingDto} from "../../_models/metadata/age-rating-dto";
 
 
 @Component({
@@ -20,7 +24,8 @@ import {map} from "rxjs/operators";
     SettingSwitchComponent,
     SettingItemComponent,
     DefaultValuePipe,
-    TagBadgeComponent
+    TagBadgeComponent,
+    AgeRatingPipe
   ],
   templateUrl: './manage-metadata-settings.component.html',
   styleUrl: './manage-metadata-settings.component.scss',
@@ -29,14 +34,24 @@ import {map} from "rxjs/operators";
 export class ManageMetadataSettingsComponent implements OnInit {
 
   private readonly settingService = inject(SettingsService);
+  private readonly metadataService = inject(MetadataService);
   private readonly cdRef = inject(ChangeDetectorRef);
   private readonly destroyRef = inject(DestroyRef);
+  private readonly fb = inject(FormBuilder);
 
   settingsForm: FormGroup = new FormGroup({});
+  ageRatings: Array<AgeRatingDto> = [];
+  ageRatingMappings = this.fb.array([]);
 
 
   ngOnInit(): void {
+    this.metadataService.getAllAgeRatings().subscribe(ratings => {
+      this.ageRatings = ratings;
+      this.cdRef.markForCheck();
+    });
 
+
+    this.settingsForm.addControl('ageRatingMappings', this.ageRatingMappings);
     this.settingService.getMetadataSettings().subscribe(settings => {
       this.settingsForm.addControl('enableSummary', new FormControl(settings.enableSummary, []));
       this.settingsForm.addControl('enablePublicationStatus', new FormControl(settings.enablePublicationStatus, []));
@@ -48,6 +63,11 @@ export class ManageMetadataSettingsComponent implements OnInit {
       this.settingsForm.addControl('enableStartDate', new FormControl(settings.enableStartDate, []));
 
       this.settingsForm.addControl('blacklist', new FormControl((settings.blacklist || '').join(','), []));
+      if (settings.ageRatingMappings) {
+        Object.entries(settings.ageRatingMappings).forEach(([str, rating]) => {
+          this.addAgeRatingMapping(str, rating);
+        });
+      }
 
       this.cdRef.markForCheck();
 
@@ -66,11 +86,36 @@ export class ManageMetadataSettingsComponent implements OnInit {
   packData() {
     const model = this.settingsForm.value;
 
+    // Convert FormArray to dictionary
+    const ageRatingMappings = this.ageRatingMappings.controls.reduce((acc, control) => {
+      // @ts-ignore
+      const { str, rating } = control.value;
+      if (str && rating) {
+        // @ts-ignore
+        acc[str] = parseInt(rating + '', 10) as AgeRating;
+      }
+      return acc;
+    }, {});
+
     // Translate blacklist string -> Array<string>
     return {
       ...model,
+      ageRatingMappings,
       blacklist: (model.blacklist || '').split(',').map((item: string) => item.trim())
     }
+  }
+
+  addAgeRatingMapping(str: string = '', rating: AgeRating = AgeRating.Unknown) {
+    const mappingGroup = this.fb.group({
+      str: [str, Validators.required],
+      rating: [rating, Validators.required]
+    });
+    // @ts-ignore
+    this.ageRatingMappings.push(mappingGroup);
+  }
+
+  removeAgeRatingMappingRow(index: number) {
+    this.ageRatingMappings.removeAt(index);
   }
 
 
