9fdf0ed662c1e6bb155203555718fc29e03d9870
Midhun Sudhir <60651970+midhun3301@users.noreply.github.com>
2025-02-06 19:24:31 +0530

    [skip ci] Fixed typo in scanner error message popover (#3470)

--- API/Services/Tasks/ScannerService.cs
+++ API/Services/Tasks/ScannerService.cs
@@ -450,12 +450,12 @@ public class ScannerService : IScannerService
             // That way logging and UI informing is all in one place with full context
             _logger.LogError("[ScannerService] Some of the root folders for the library are empty. " +
                              "Either your mount has been disconnected or you are trying to delete all series in the library. " +
-                             "Scan has be aborted. " +
+                             "Scan has been aborted. " +
                              "Check that your mount is connected or change the library's root folder and rescan");
 
             await _eventHub.SendMessageAsync(MessageFactory.Error, MessageFactory.ErrorEvent( $"Some of the root folders for the library, {libraryName}, are empty.",
                 "Either your mount has been disconnected or you are trying to delete all series in the library. " +
-                "Scan has be aborted. " +
+                "Scan has been aborted. " +
                 "Check that your mount is connected or change the library's root folder and rescan"));
 
             return false;
