d1a8796db29b5e5f83be808faf07c5001981d965
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2025-02-28 18:12:23 -0600

    Don't scrobble an error when a series is blacklisted (aka couldn't match), just fail silently

--- API/Services/Plus/ScrobblingService.cs
+++ API/Services/Plus/ScrobblingService.cs
@@ -930,18 +930,20 @@ public class ScrobblingService : IScrobblingService
 
             if (await _unitOfWork.ExternalSeriesMetadataRepository.IsBlacklistedSeries(evt.SeriesId))
             {
-                _unitOfWork.ScrobbleRepository.Attach(new ScrobbleError()
-                {
-                    Comment = UnknownSeriesErrorMessage,
-                    Details = $"User: {evt.AppUser.UserName} Series: {evt.Series.Name}",
-                    LibraryId = evt.LibraryId,
-                    SeriesId = evt.SeriesId
-                });
-                evt.IsErrored = true;
-                evt.ErrorDetails = UnknownSeriesErrorMessage;
-                evt.ProcessDateUtc = DateTime.UtcNow;
-                _unitOfWork.ScrobbleRepository.Update(evt);
-                await _unitOfWork.CommitAsync();
+                // I'm not sure why I was storing a scrobble error when blacklisted, but this should now address
+                // https://github.com/Kareadita/Kavita/issues/3348
+                // _unitOfWork.ScrobbleRepository.Attach(new ScrobbleError()
+                // {
+                //     Comment = UnknownSeriesErrorMessage,
+                //     Details = $"User: {evt.AppUser.UserName} Series: {evt.Series.Name}",
+                //     LibraryId = evt.LibraryId,
+                //     SeriesId = evt.SeriesId
+                // });
+                // evt.IsErrored = true;
+                // evt.ErrorDetails = UnknownSeriesErrorMessage;
+                // evt.ProcessDateUtc = DateTime.UtcNow;
+                // _unitOfWork.ScrobbleRepository.Update(evt);
+                // await _unitOfWork.CommitAsync();
                 return 0;
             }
 
