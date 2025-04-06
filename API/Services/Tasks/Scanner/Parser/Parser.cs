0563e7931830c645161ceea632a355c5b8269613
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2025-04-06 10:41:09 -0500

    Code cleanup

--- API/Services/Tasks/Scanner/Parser/Parser.cs
+++ API/Services/Tasks/Scanner/Parser/Parser.cs
@@ -24,7 +24,7 @@ public static partial class Parser
 
     public static readonly TimeSpan RegexTimeout = TimeSpan.FromMilliseconds(500);
 
-    public const string ImageFileExtensions = @"^(\.png|\.jpeg|\.jpg|\.webp|\.gif|\.avif)"; // Don't forget to update CoverChooser
+    public const string ImageFileExtensions = @"(\.png|\.jpeg|\.jpg|\.webp|\.gif|\.avif)"; // Don't forget to update CoverChooser
     public const string ArchiveFileExtensions = @"\.cbz|\.zip|\.rar|\.cbr|\.tar.gz|\.7zip|\.7z|\.cb7|\.cbt";
     public const string EpubFileExtension = @"\.epub";
     public const string PdfFileExtension = @"\.pdf";
