bced614481ffce813c5172f9cd485298cfdb99bf
Amelia <77553571+Fesaa@users.noreply.github.com>
2025-06-12 11:47:19 +0200

    Revert badly commit change in earlier PR

--- Kavita.Common/Configuration.cs
+++ Kavita.Common/Configuration.cs
@@ -17,7 +17,7 @@ public static class Configuration
     private static readonly string AppSettingsFilename = Path.Join("config", GetAppSettingFilename());
 
     public static readonly string KavitaPlusApiUrl = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Development
-        ?  "https://plus.kavitareader.com" : "https://plus.kavitareader.com";
+        ?  "http://localhost:5020" : "https://plus.kavitareader.com";
     public static readonly string StatsApiUrl = "https://stats.kavitareader.com";
 
     public static int Port
