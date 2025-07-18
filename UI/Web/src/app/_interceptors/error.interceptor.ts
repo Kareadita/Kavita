9a529ee674226d92e00747326b448f8ec9019d2b
Amelia <77553571+Fesaa@users.noreply.github.com>
2025-07-18 21:09:59 +0200

    Fix endless login loop when failing to login with OIDC with auto login enabled

--- UI/Web/src/app/_interceptors/error.interceptor.ts
+++ UI/Web/src/app/_interceptors/error.interceptor.ts
@@ -31,7 +31,7 @@ export class ErrorInterceptor implements HttpInterceptor {
             this.handleValidationError(error);
             break;
           case 401:
-            this.handleAuthError(error);
+            this.handleAuthError(request, error);
             break;
           case 404:
             this.handleNotFound(error);
@@ -119,7 +119,7 @@ export class ErrorInterceptor implements HttpInterceptor {
     console.error('500 error:', error);
   }
 
-  private handleAuthError(error: any) {
+  private handleAuthError(req: HttpRequest<unknown>, error: any) {
     // Special hack for register url, to not care about auth
     if (location.href.includes('/registration/confirm-email?token=')) {
       return;
@@ -130,9 +130,13 @@ export class ErrorInterceptor implements HttpInterceptor {
       localStorage.setItem(AuthGuard.urlKey, path);
     }
 
+    if (error.error && error.error !== 'Unauthorized') {
+      this.toastr.error(error.error);
+    }
+
     // NOTE: Signin has error.error or error.statusText available.
     // if statement is due to http/2 spec issue: https://github.com/angular/angular/issues/23334
-    this.accountService.logout();
+    this.accountService.logout(req.method === 'GET' && req.url.endsWith('/api/account'));
   }
 
   // Assume the title is already translated
