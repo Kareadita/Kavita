a197f3827e18eb917cdad9fe28fb61db6294b815
Amelia <77553571+Fesaa@users.noreply.github.com>
2025-08-03 23:17:44 +0200

    Add a long click directive

---
+++ UI/Web/src/app/_directives/long-click.directive.ts
@@ -0,0 +1,45 @@
+import {Directive, ElementRef, EventEmitter, inject, input, OnDestroy, Output} from '@angular/core';
+import {fromEvent, merge, Subscription, switchMap, timer} from "rxjs";
+import {takeUntil} from "rxjs/operators";
+
+@Directive({
+  selector: '[appLongClick]',
+  standalone: true
+})
+export class LongClickDirective implements OnDestroy {
+
+  private elementRef: ElementRef = inject(ElementRef);
+
+  private readonly eventSubscribe: Subscription;
+
+  /**
+   * How long should the element be pressed for
+   * @default 500
+   */
+  threshold = input(500);
+
+  @Output() longClick = new EventEmitter();
+
+  constructor() {
+    const start$ = merge(
+      fromEvent(this.elementRef.nativeElement, 'touchstart'),
+      fromEvent(this.elementRef.nativeElement, 'mousedown')
+    );
+
+    const end$ = merge(
+      fromEvent(this.elementRef.nativeElement, 'touchend'),
+      fromEvent(this.elementRef.nativeElement, 'mouseup')
+    );
+
+    this.eventSubscribe = start$
+      .pipe(switchMap(() => timer(this.threshold()).pipe(takeUntil(end$))))
+      .subscribe(() => this.longClick.emit());
+  }
+
+  ngOnDestroy(): void {
+    if (this.eventSubscribe) {
+      this.eventSubscribe.unsubscribe();
+    }
+  }
+
+}
