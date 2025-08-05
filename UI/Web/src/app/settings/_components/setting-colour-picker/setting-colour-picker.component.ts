8f07eb907af85f3aaa7aa58e49be876a3765894b
Amelia <77553571+Fesaa@users.noreply.github.com>
2025-08-05 12:49:24 +0200

    Auto close colour picker if clicking outside of it

--- UI/Web/src/app/settings/_components/setting-colour-picker/setting-colour-picker.component.ts
+++ UI/Web/src/app/settings/_components/setting-colour-picker/setting-colour-picker.component.ts
@@ -1,4 +1,16 @@
-import {Component, EventEmitter, inject, input, model, OnInit, Output, signal} from '@angular/core';
+import {
+  Component,
+  effect, ElementRef,
+  EventEmitter,
+  HostListener,
+  inject,
+  input,
+  model,
+  OnInit,
+  Output,
+  signal,
+  ViewChild
+} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SlotColorPipe} from "../../../_pipes/slot-color.pipe";
 import {RgbaColor} from "../../../book-reader/_models/annotations/highlight-slot";
@@ -15,8 +27,11 @@ import {UserBreakpoint, UtilityService} from "../../../shared/_services/utility.
 })
 export class SettingColourPickerComponent implements OnInit {
 
+  private readonly elementRef = inject(ElementRef);
   private readonly slotColorPipe = inject(SlotColorPipe);
   private readonly utilityService: UtilityService = inject(UtilityService);
+
+  @ViewChild('colourPopup') colourPopup?: ElementRef;
 
   editMode = model(false);
   color = input.required<RgbaColor>();
@@ -24,10 +39,23 @@ export class SettingColourPickerComponent implements OnInit {
 
   showPicker = signal(false);
 
-  chromeControl!: ColorPickerControl;
-
   @Output() colorChange = new EventEmitter<string>();
   @Output() selectPicker = new EventEmitter<void>();
+
+  chromeControl!: ColorPickerControl;
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event): void {
+    if (!this.showPicker()) return;
+
+    if (!this.colourPopup) return;
+
+    const clickedElement = event.target as Node;
+
+    if (!this.elementRef.nativeElement.contains(clickedElement) && !this.colourPopup.nativeElement.contains(clickedElement)) {
+      this.showPicker.set(false);
+    }
+  }
 
   onColorChange(color: string): void {
     this.colorChange.emit(color);
@@ -46,9 +74,7 @@ export class SettingColourPickerComponent implements OnInit {
   }
 
   togglePicker(): void {
-    if (this.utilityService.activeUserBreakpoint() >= UserBreakpoint.Desktop) {
-      this.showPicker.update(b => !b);
-    }
+    this.showPicker.update(b => !b);
   }
 
   ngOnInit(): void {
