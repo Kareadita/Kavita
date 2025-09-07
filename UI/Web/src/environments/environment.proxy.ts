0977ef98543fcbe0ae186a13a966d00f2be88014
Amelia <77553571+Fesaa@users.noreply.github.com>
2025-09-07 12:39:03 +0200

    Add special proxy configuration for OIDC development

---
+++ UI/Web/src/environments/environment.proxy.ts
@@ -0,0 +1,22 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+const IP = 'localhost';
+
+export const environment = {
+  production: false,
+  apiUrl: 'http://' + IP + ':4200/api/',
+  hubUrl: 'http://'+ IP + ':4200/hubs/',
+  buyLink: 'https://buy.stripe.com/test_9AQ5mi058h1PcIo3cf?prefilled_promo_code=FREETRIAL',
+  manageLink: 'https://billing.stripe.com/p/login/test_14kfZocuh6Tz5ag7ss'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
