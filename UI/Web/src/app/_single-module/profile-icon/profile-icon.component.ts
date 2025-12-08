06d7f0389655ff9a609b04e7b7301d828d5ae9a9
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2025-12-08 06:59:00 -0600

    Fixed updating profile picture lagging out the page

--- UI/Web/src/app/_single-module/profile-icon/profile-icon.component.ts
+++ UI/Web/src/app/_single-module/profile-icon/profile-icon.component.ts
@@ -1,8 +1,9 @@
-import {ChangeDetectionStrategy, Component, effect, inject, input, model} from '@angular/core';
+import {ChangeDetectionStrategy, Component, DestroyRef, effect, inject, input, model} from '@angular/core';
 import {ImageService} from "../../_services/image.service";
 import {ImageComponent} from "../../shared/image/image.component";
 import {EVENTS, MessageHubService} from "../../_services/message-hub.service";
 import {CoverUpdateEvent} from "../../_models/events/cover-update-event";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-profile-icon',
@@ -16,6 +17,7 @@ import {CoverUpdateEvent} from "../../_models/events/cover-update-event";
 export class ProfileIconComponent {
   protected readonly imageService = inject(ImageService);
   protected readonly hubService = inject(MessageHubService);
+  protected readonly destroyRef = inject(DestroyRef);
 
   userId = input.required<number>();
 
@@ -27,40 +29,36 @@ export class ProfileIconComponent {
 
   currentImageUrl = model<string>('');
 
-
-  // currentImageUrl = computed(() => {
-  //   const userId = this.userId();
-  //   return this.imageService.getUserCoverImage(userId);
-  // });
-
   constructor() {
 
+    this.hubService.messages$.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(res => {
+      if (!this.processEvents()) return;
+      const imageUrl = this.currentImageUrl();
+
+      if (res.event === EVENTS.CoverUpdate) {
+        const updateEvent = res.payload as CoverUpdateEvent;
+        if (imageUrl === undefined || imageUrl === null || imageUrl === '') return;
+        const entityType = this.imageService.getEntityTypeFromUrl(imageUrl);
+        if (entityType === updateEvent.entityType) {
+          const tokens = imageUrl.split('?')[1].split('&');
+
+          //...seriesId=123&random=
+          let id = tokens[0].replace(entityType + 'Id=', '');
+          if (id.includes('&')) {
+            id = id.split('&')[0];
+          }
+          if (id === (updateEvent.id + '')) {
+            this.currentImageUrl.set(this.imageService.randomize(imageUrl))
+          }
+        }
+      }
+    });
 
     effect(() => {
       const userId = this.userId();
-      const res = this.hubService.messageSignal();
 
       // Set default image
       this.currentImageUrl.set(this.imageService.getUserCoverImage(userId));
-
-      if (res?.event !== EVENTS.CoverUpdate) return;
-
-      const updateEvent = res.payload as CoverUpdateEvent;
-      const imageUrl = this.currentImageUrl();
-      if (imageUrl === undefined || imageUrl === null || imageUrl === '') return;
-      const entityType = this.imageService.getEntityTypeFromUrl(imageUrl);
-      if (entityType === updateEvent.entityType) {
-        const tokens = imageUrl.split('?')[1].split('&');
-
-        //...userId=123&random=
-        let id = tokens[0].replace(entityType + 'Id=', '');
-        if (id.includes('&')) {
-          id = id.split('&')[0];
-        }
-        if (id === (updateEvent.id + '')) {
-          this.currentImageUrl.set(this.imageService.randomize(imageUrl));
-        }
-      }
     });
 
   }
