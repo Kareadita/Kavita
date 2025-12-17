470b148b4d2842f59313103ee69031758cd27653
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2025-12-17 16:14:32 -0600

    Better null handling

--- API/Controllers/MetadataController.cs
+++ API/Controllers/MetadataController.cs
@@ -257,10 +257,13 @@ public class MetadataController(IUnitOfWork unitOfWork, IExternalMetadataService
         var isAdmin = User.IsInRole(PolicyConstants.AdminRole);
         var user = await unitOfWork.UserRepository.GetUserByIdAsync(UserId)!;
 
-        userReviews.AddRange(ReviewHelper.SelectSpectrumOfReviews(ret.Reviews.ToList()));
-        ret.Reviews = userReviews;
+        if (ret != null)
+        {
+            userReviews.AddRange(ReviewHelper.SelectSpectrumOfReviews(ret.Reviews.ToList()));
+            ret.Reviews = userReviews;
+        }
 
-        if (!isAdmin && ret.Recommendations != null && user != null)
+        if (!isAdmin && ret?.Recommendations != null && user != null)
         {
             // Re-obtain owned series and take into account age restriction
             ret.Recommendations.OwnedSeries =
