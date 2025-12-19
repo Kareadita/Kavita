bde6e3054496166d821486db295d359a70f1fe3c
Lucas Winther <lucasw89@live.dk>
2025-12-19 01:31:55 +0100

    Move the PDF Render Resolution Extension to its own file.

--- API/Entities/Enums/PdfRenderResolution.cs
+++ API/Entities/Enums/PdfRenderResolution.cs
@@ -15,16 +15,3 @@ public enum PdfRenderResolution
     /// </summary>
     Ultra = 3,
 }
-public static class PdfRenderResolutionExtensions
-{
-    public static (int dim1, int dim2) GetDimensions(this PdfRenderResolution size)
-    {
-        return size switch
-        {
-            PdfRenderResolution.Default => (1080, 1920),
-            PdfRenderResolution.High => (1920, 2560),
-            PdfRenderResolution.Ultra => (2160, 3840),
-            _ => (1080, 1920)
-        };
-    }
-}
