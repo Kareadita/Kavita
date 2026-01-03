5310a0292e14fb1bba3a9df769a074066b8b1790
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2026-01-03 15:14:42 -0600

    All tables in the app will now convert to cards on mobile.

--- UI/Web/src/app/admin/manage-library/manage-library.component.ts
+++ UI/Web/src/app/admin/manage-library/manage-library.component.ts
@@ -5,7 +5,8 @@ import {
   DestroyRef,
   HostListener,
   inject,
-  OnInit
+  OnInit,
+  TrackByFunction
 } from '@angular/core';
 import {NgbModal, NgbTooltip} from '@ng-bootstrap/ng-bootstrap';
 import {ToastrService} from 'ngx-toastr';
@@ -38,6 +39,13 @@ import {
 } from "../_modals/copy-settings-from-library-modal/copy-settings-from-library-modal.component";
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {SelectionModel} from "../../typeahead/_models/selection-model";
+import {ResponsiveTableComponent} from "../../shared/_components/responsive-table/responsive-table.component";
+import {
+  DataTableColumnCellDirective,
+  DataTableColumnDirective,
+  DataTableColumnHeaderDirective,
+  DatatableComponent
+} from "@siemens/ngx-datatable";
 
 @Component({
   selector: 'app-manage-library',
@@ -45,7 +53,7 @@ import {SelectionModel} from "../../typeahead/_models/selection-model";
   styleUrls: ['./manage-library.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [RouterLink, NgbTooltip, LibraryTypePipe, TimeAgoPipe, SentenceCasePipe, TranslocoModule, DefaultDatePipe,
-    AsyncPipe, LoadingComponent, CardActionablesComponent, NgTemplateOutlet, ReactiveFormsModule, FormsModule]
+    AsyncPipe, LoadingComponent, CardActionablesComponent, NgTemplateOutlet, ReactiveFormsModule, FormsModule, ResponsiveTableComponent, DatatableComponent, DataTableColumnHeaderDirective, DataTableColumnDirective, DataTableColumnCellDirective]
 })
 export class ManageLibraryComponent implements OnInit {
 
@@ -81,6 +89,9 @@ export class ManageLibraryComponent implements OnInit {
   bulkForm = new FormGroup({'includeType': new FormControl(false)});
   isShiftDown: boolean = false;
   lastSelectedIndex: number | null = null;
+
+  trackByLibrary: TrackByFunction<Library> = (_, lib) =>
+    `${lib.name}_${lib.type}_${lib.folders.length}_${lib.lastScanned}`;
 
   @HostListener('document:keydown.shift', ['$event'])
   handleKeypress(_: Event) {
