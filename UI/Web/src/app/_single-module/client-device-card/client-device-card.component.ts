bfba6dc9b48107f8cf2e15b093656d61e5184199
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2026-01-08 08:05:33 -0600

    Fixed not being able to edit device name on mobile

--- UI/Web/src/app/_single-module/client-device-card/client-device-card.component.ts
+++ UI/Web/src/app/_single-module/client-device-card/client-device-card.component.ts
@@ -19,6 +19,7 @@ import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/
 import {DOCUMENT} from "@angular/common";
 import {AccountService} from "../../_services/account.service";
 import {User} from "../../_models/user/user";
+import {Breakpoint, UtilityService} from "../../shared/_services/utility.service";
 
 @Component({
   selector: 'app-client-device-card',
@@ -46,6 +47,7 @@ export class ClientDeviceCardComponent {
   private readonly deviceService = inject(DeviceService);
   private readonly document = inject(DOCUMENT);
   private readonly accountService = inject(AccountService);
+  private readonly utilityService = inject(UtilityService);
   protected readonly clientInfoService = inject(ClientInfoService);
 
   clientDevice = input.required<ClientDevice>();
@@ -171,7 +173,12 @@ export class ClientDeviceCardComponent {
         this.deleteDevice();
         break;
       case Action.Edit:
-        this.toggleEdit();
+        // The actionable modal needs some time to clean up
+        if (this.utilityService.activeBreakpointSignal()! < Breakpoint.Tablet ) {
+          setTimeout(() => this.toggleEdit(), 100);
+        } else {
+          this.toggleEdit();
+        }
         break;
     }
   }
