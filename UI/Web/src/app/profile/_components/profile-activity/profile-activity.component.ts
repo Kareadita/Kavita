e96e5dd109dff59977ab245a32859230c449fce4
Amelia <77553571+Fesaa@users.noreply.github.com>
2026-01-10 14:56:28 +0100

    Allow clicking on chapter entries to navigate to them

--- UI/Web/src/app/profile/_components/profile-activity/profile-activity.component.ts
+++ UI/Web/src/app/profile/_components/profile-activity/profile-activity.component.ts
@@ -163,14 +163,14 @@ export class ProfileActivityComponent {
   }
 
   protected displayInfo(item: ReadingHistoryItem): void {
-    const [_, component] = this.modalService.open(ListSelectModalComponent<ReadingHistoryChapterItem>, {
+    const [_, component] = this.modalService.open(ListSelectModalComponent<{entry: ReadingHistoryItem, chapter: ReadingHistoryChapterItem}>, {
       size: 'lg',
       centered: true
     });
 
     component.title.set(item.seriesName);
     component.showConfirm.set(false);
-    component.inputItems.set(item.chapters.map(c => ({ value: c, label: `${c.label}` })));
+    component.inputItems.set(item.chapters.map(c => ({ value: {entry: item, chapter: c}, label: `${c.label}` })));
     component.itemTemplate.set(this.chapterInfoRow());
     component.itemsBeforeVirtual.set(5);
   }
