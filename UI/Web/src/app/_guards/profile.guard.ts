59d792a29863f5b40d26f5b0177a1e1821ec3b95
Amelia <77553571+Fesaa@users.noreply.github.com>
2026-01-12 01:31:56 +0100

    Redirect to home when accessing a non-shared profile

--- UI/Web/src/app/_guards/profile.guard.ts
+++ UI/Web/src/app/_guards/profile.guard.ts
@@ -1,16 +1,18 @@
-import {CanActivateFn} from '@angular/router';
+import {CanActivateFn, RedirectCommand, Router} from '@angular/router';
 import {AccountService} from "../_services/account.service";
 import {inject} from "@angular/core";
 import {MemberService} from "../_services/member.service";
 import {ToastrService} from "ngx-toastr";
 import {translate} from "@jsverse/transloco";
 import {tap} from "rxjs";
+import {map} from "rxjs/operators";
 
 export const profileGuard: CanActivateFn = (route, state) => {
   const userId = parseInt(route.params['userId'] || route.parent?.params['userId'], 10);
 
   const accountService = inject(AccountService);
   const memberService = inject(MemberService);
+  const router = inject(Router);
   const toastr = inject(ToastrService);
 
   // If this is my profile, allow
@@ -21,10 +23,17 @@ export const profileGuard: CanActivateFn = (route, state) => {
   // Otherwise check if that user has their account shared
   return memberService.hasProfileShared(userId).pipe(
     tap(hasAccess => {
-      console.log('hasAccess', hasAccess);
       if (!hasAccess) {
         toastr.info(translate('toasts.profile-unauthorized'));
       }
+    }),
+    map(hasAccess => {
+      if (hasAccess) {
+        return true;
+      }
+
+      const dashboard = router.parseUrl('/home');
+      return new RedirectCommand(dashboard);
     })
   );
 };
