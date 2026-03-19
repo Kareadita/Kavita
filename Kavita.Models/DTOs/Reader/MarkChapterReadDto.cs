339051cd34b8fed3b21e0595df774f69844808cd
Amelia <77553571+Fesaa@users.noreply.github.com>
2026-03-19 18:26:04 +0100

    Add some documentation on the generate reading session fields

--- Kavita.Models/DTOs/Reader/MarkChapterReadDto.cs
+++ Kavita.Models/DTOs/Reader/MarkChapterReadDto.cs
@@ -4,5 +4,9 @@ public class MarkChapterReadDto
 {
     public int SeriesId { get; init; }
     public int ChapterId { get; init; }
+    /// <summary>
+    /// If true, generates a new reading session for the user. Based on the estimated time from the current progress
+    /// till the end
+    /// </summary>
     public bool GenerateReadingSession { get; init; }
 }
--- Kavita.Models/DTOs/Reader/MarkReadDto.cs
+++ Kavita.Models/DTOs/Reader/MarkReadDto.cs
@@ -3,5 +3,9 @@
 public sealed record MarkReadDto
 {
     public int SeriesId { get; init; }
+    /// <summary>
+    /// If true, generates a new reading session for the user. Based on the estimated time from the current progress
+    /// till the end
+    /// </summary>
     public bool GenerateReadingSession { get; init; }
 }
