884ca144d7dccdd4d9657069a355469ded336ad4
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2026-05-18 12:00:06 -0500

    Prep before building design

--- UI/Web/src/app/admin/manage-kavitaplus-activity/manage-kavitaplus-activity.component.ts
+++ UI/Web/src/app/admin/manage-kavitaplus-activity/manage-kavitaplus-activity.component.ts
@@ -1,8 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {TranslocoDirective} from "@jsverse/transloco";
 
 @Component({
   selector: 'app-manage-kavitaplus-activity',
-  imports: [],
+  imports: [
+    TranslocoDirective
+  ],
   templateUrl: './manage-kavitaplus-activity.component.html',
   styleUrl: './manage-kavitaplus-activity.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
