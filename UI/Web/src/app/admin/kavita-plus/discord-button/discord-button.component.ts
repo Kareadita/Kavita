19974c099348472b89257a4a28bed86975996de8
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2026-05-22 08:37:58 -0500

    Added a styled discord button

---
+++ UI/Web/src/app/admin/kavita-plus/discord-button/discord-button.component.ts
@@ -0,0 +1,16 @@
+import {ChangeDetectionStrategy, Component, input} from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-discord-button',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './discord-button.component.html',
+  styleUrl: './discord-button.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DiscordButtonComponent {
+  href = input<string>('');
+  label = input<string>('Connect');
+}
