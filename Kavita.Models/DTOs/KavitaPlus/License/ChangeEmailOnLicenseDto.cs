a09b03b00e2671ba9a711743e180978bb1fbc3a9
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2026-06-16 06:49:34 -0500

    oops forgot a class

---
+++ Kavita.Models/DTOs/KavitaPlus/License/ChangeEmailOnLicenseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Kavita.Models.DTOs.KavitaPlus.License;
+
+public sealed record ChangeEmailOnLicenseDto
+{
+    public required string ExistingEmail { get; set; }
+    public required string NewEmail { get; set; }
+
+}
