8ae67c65cfe7a4644c1cfe545a1cf0574411a053
Amelia <77553571+Fesaa@users.noreply.github.com>
2026-09-23 11:55:54 +0200

    Add migrations for OnDeckProgressDays and OnDeckUpdateDays

---
+++ Kavita.Database/Migrations/20260923094214_AddMissingUserPreferenceDefaults.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kavita.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissingUserPreferenceDefaults : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "OnDeckUpdateDays",
+                table: "AppUserPreferences",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 7,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OnDeckProgressDays",
+                table: "AppUserPreferences",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 30,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "OnDeckUpdateDays",
+                table: "AppUserPreferences",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldDefaultValue: 7);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OnDeckProgressDays",
+                table: "AppUserPreferences",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldDefaultValue: 30);
+        }
+    }
+}
